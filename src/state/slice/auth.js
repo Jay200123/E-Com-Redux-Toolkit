201fe366b1826d345a5fb8abfd6d59a71ed4ec28
Jay200123 <kenken.ayonon@gmail.com>
2025-01-13 17:44:33 +0800

    Add authentication slice to manage user state and login status

---
+++ src/state/slice/auth.js
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { api } from "../api/index";
+
+const initialState = {
+  user: null,
+  token: null,
+  isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    logout: {
+      reducer(state) {
+        state.user = null;
+        state.token = null;
+        state.isAuthenticated = false;
+      },
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addMatcher(api.endpoints.login.matchFulfilled, (state, action) => {
+      state.user = action.payload.details;
+      state.token = action.payload.access;
+      state.isAuthenticated = true;
+    });
+
+    builder.addMatcher(api.endpoints.login.matchRejected, (state) => {
+      state.user = null;
+      state.token = null;
+      state.isAuthenticated = false;
+    });
+  },
+});
+
+export const { logout } = authSlice.actions;
+export default authSlice.reducer;
