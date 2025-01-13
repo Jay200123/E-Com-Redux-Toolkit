7b6ba0d1ed661d3af8c249bdef97b22bbd67bcbe
Jay200123 <kenken.ayonon@gmail.com>
2025-01-13 17:44:17 +0800

    Add Redux store configuration with persistence support

---
+++ src/state/store.js
@@ -0,0 +1,34 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { rootReducer } from "./reducer";
+import { persistConfig } from "./persist";
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [
+          FLUSH,
+          REHYDRATE,
+          PAUSE,
+          PERSIST,
+          PURGE,
+          REGISTER
+        ],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
