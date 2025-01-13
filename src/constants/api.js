b46ae21c444368612c11d1ca9df61ac30c75a559
Jay200123 <kenken.ayonon@gmail.com>
2025-01-13 17:43:35 +0800

    Add API constants for HTTP methods and tags

---
+++ src/constants/api.js
@@ -0,0 +1,13 @@
+import TAGS from "./tags";
+
+export default {
+  GET: "get",
+  POST: "POST",
+  PATCH: "PATCH",
+  PUT: "PUT",
+  DELETE: "DELETE",
+  TAGS: [
+    TAGS.API,
+    TAGS.AUTH
+  ]
+};
