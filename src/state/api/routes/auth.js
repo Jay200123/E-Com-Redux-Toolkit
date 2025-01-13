ddbedc8ac9a7936fb3855d4916cfd82ba02c9c17
Jay200123 <kenken.ayonon@gmail.com>
2025-01-13 17:44:27 +0800

    Add login mutation to API slice for authentication

---
+++ src/state/api/routes/auth.js
@@ -0,0 +1,15 @@
+export const login = (builder) => {
+  return builder.mutation({
+    query: (payload) => {
+      return {
+        url: "/login",
+        method: "POST",
+        body: payload,
+      };
+    },
+  });
+};
+
+export default {
+  login,
+};
