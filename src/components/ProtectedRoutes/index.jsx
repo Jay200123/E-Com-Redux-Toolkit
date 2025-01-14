2ba7f884d52b63fa3e1ccce7dfd878aa99344973
Jay200123 <kenken.ayonon@gmail.com>
2025-01-14 20:53:16 +0800

    Add ProtectedRoutes component for user authentication and role authorization

---
+++ src/components/ProtectedRoutes/index.jsx
@@ -0,0 +1,22 @@
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+
+export default function({ children, userRole }) {
+  const auth = useSelector((state) => state.auth);
+
+  if (!auth?.isAuthenticated) {
+    return <Navigate to="/login" />;
+  }
+
+  if (userRole) {
+    const isRoleAuthorized = Array.isArray(userRole)
+      ? userRole.includes(auth?.user?.role)
+      : auth?.user?.role === userRole;
+
+    if (!isRoleAuthorized) {
+      return <Navigate to="/unauthorized" />;
+    }
+  }
+
+  return <>{children}</>;
+};
