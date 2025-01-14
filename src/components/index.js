407ee144139b265fb4f8efff760eb332480f15e1
Jay200123 <kenken.ayonon@gmail.com>
2025-01-14 20:53:07 +0800

    Add ProtectedRoutes component to index export

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,9 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import ProtectedRoutes from "./ProtectedRoutes";
 
-export { Navbar, Footer };
+export { 
+    Navbar,
+    Footer,
+    ProtectedRoutes
+};
