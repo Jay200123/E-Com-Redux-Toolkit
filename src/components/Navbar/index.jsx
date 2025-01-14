8c8ba987fd42bb9877c7b0ce6c12f36c3ec2bf36
Jay200123 <kenken.ayonon@gmail.com>
2025-01-14 20:53:11 +0800

    Implement logout functionality in Navbar with success notification

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,7 +1,19 @@
-export default function(){
-    return(
-        <>
-        this is a navbar
-        </>
-    )
+import { logout } from "../../state/slice/auth";
+import { useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+
+export default function () {
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(logout());
+    toast.success("Logged out successfully");
+  };
+  return <nav className="flex items-center justify-between w-full h-16 shadow-md md:flex-row" onClick={handleLogout}>
+    <div>React</div>
+    <ul className="flex flex-row items-center">
+      <li className="mr-1">LOGIN</li>
+      <li className="mr-1">SIGN UP</li>
+    </ul>
+  </nav>;
 }
