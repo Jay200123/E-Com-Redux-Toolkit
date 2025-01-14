3c345c927da67b64bd20ae146894bd06619d6249
Jay200123 <kenken.ayonon@gmail.com>
2025-01-14 20:53:02 +0800

    Add ToastContainer for notifications in the application

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./state/store.js";
+import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
+        <ToastContainer/>
         <App />
       </PersistGate>
     </Provider>
