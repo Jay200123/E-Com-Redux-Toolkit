2f99cd6cd702d8b6aa826ff4d41ca322cd2df4fa
Jay200123 <kenken.ayonon@gmail.com>
2025-01-15 21:00:53 +0800

    Add cart slice with reducers for managing cart items

---
+++ src/state/slice/cart.js
@@ -0,0 +1,68 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { act } from "react";
+
+const initialState = {
+  item: [],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addCart: (state, action) => {
+      const existingItem = state.item.find(
+        (item) => item.product._id === action.payload.product._id
+      );
+
+      if (existingItem) {
+        existingItem.orderQty += 1;
+      } else {
+        state.item.push({
+          ...action.payload,
+          product: action.payload.product,
+          orderQty: action.payload.quantity,
+        });
+      }
+    },
+    increaseQuantity: (state, action) => {
+      const existingItem = state.item.find(
+        (item) => item.product._id === action.payload
+      );
+
+      if (existingItem) {
+        existingItem.orderQty += 1;
+      }
+    },
+    decreaseQuantity: (state, action) => {
+      const existingItem = state.item.find(
+        (item) => item.product._id === action.payload
+      );
+
+      if (existingItem) {
+        existingItem.orderQty - 1;
+      }
+    },
+    removeCart: (state, action) => {
+      state.item = state.item.filter(
+        (item) => item.product._id !== action.payload
+      );
+    },
+    clearCart: (state) => {
+      state.item = [];
+    },
+    setState: () => {
+      return initialState;
+    },
+  },
+});
+
+export const {
+  addCart,
+  increaseQuantity,
+  decreaseQuantity,
+  removeCart,
+  clearState,
+  setState,
+  clearCart,
+} = cartSlice.actions;
+export default cartSlice.reducer;
