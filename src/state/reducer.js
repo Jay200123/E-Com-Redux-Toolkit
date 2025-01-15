608ac7e83995ca4a1fb16c39af1073e0925b4577
Jay200123 <kenken.ayonon@gmail.com>
2025-01-15 21:00:26 +0800

    Add Cart reducer and API integration to root reducer

--- src/state/reducer.js
+++ src/state/reducer.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import AuthReducer from "./slice/auth";
+import CartReducer from "./slice/cart";
+import { api } from "./api/reducer";
 
 export const rootReducer = combineReducers({
   auth: AuthReducer,
+  cart: CartReducer,
+  [api.reducerPath]: api.reducer,
 });
