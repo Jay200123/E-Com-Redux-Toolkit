52a4a7c73b5a8b699dffcf9bed1361a8f21a3f0a
Jay200123 <kenken.ayonon@gmail.com>
2025-01-15 21:00:07 +0800

    Add Register component to index export for routing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import Home from "./Home";
 import Login from "./Login";	 
+import Register from "./Register";
 import ProtectedPage from "./ProtectedPage";   
 
 export {
      Home,
      Login,
+     Register,
      ProtectedPage,
 }
