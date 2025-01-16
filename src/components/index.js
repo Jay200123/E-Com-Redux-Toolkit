98a5541d724b0542d82dee31c209d8b9de28ed65
Jay200123 <kenken.ayonon@gmail.com>
2025-01-16 19:26:41 +0800

    Add Carousel component to index export

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import Carousel from "./Carousel";
 import ProtectedRoutes from "./ProtectedRoutes";
 
 export { 
     Navbar,
     Footer,
+    Carousel,
     ProtectedRoutes
 };
