f902b4317162cdb0d60d9e9cbe8c5ad1b85f0d92
Jay200123 <kenken.ayonon@gmail.com>
2025-01-18 19:42:28 +0800

    Add Profile component to display user information and random profile image

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,52 @@
+import { useSelector } from "react-redux";
+
+export default function () {
+  const user = useSelector((state) => state.auth.user);  
+  const randomImage = user?.image[Math.floor(Math.random() * user.image.length)];
+    
+  return (
+    <div className="flex items-center justify-center w-full h-full p-4 overflow-hidden">
+      <div className="flex flex-col transition-all duration-700 md:flex-row w-[75rem] border border-gray-500 shadow-lg rounded-lg h-[48rem] md:h-[24rem]">
+        <div className="w-full h-full md:w-[30%]">
+          <div className="flex flex-col items-center justify-center w-full h-full p-4">
+            <img
+              src={randomImage.url}
+              alt={randomImage.originalname}   
+              className="object-contain border border-gray-500 rounded-full w-36 h-36 md:w-60 md:h-60"
+            />
+            <button className="w-full p-2 mt-2 text-sm text-white bg-black rounded-md md:mt-4 md:text-lg">Edit Profile</button>
+          </div>
+        </div>
+        <div className="flex flex-col w-full h-full md:w-[70%] p-2">
+          <h3 className="font-bold md:text-3xl">Profile Information</h3>
+          <div className="flex flex-col w-full md:flex-row">
+            <div className="w-full p-2 md:w-1/2">
+              <p className="font-semibold">First Name</p>
+              <p>{user?.fullname}</p>
+            </div>
+            <div className="w-full p-2 md:w-1/2">
+              <p className="font-semibold">Contact Number</p>
+              <p>{user?.contact_number}</p>
+            </div>
+          </div>
+          <div className="flex flex-col w-full md:flex-row ">
+            <div className="w-full p-2 md:w-1/2">
+              <p className="font-semibold">Address</p>
+              <p>{user?.address}</p>
+            </div>
+            <div className="w-full p-2 md:w-1/2">
+              <p className="font-semibold">City</p>
+              <p>{user?.city}</p>
+            </div>
+          </div>
+          <div className="flex flex-col w-full md:flex-row">
+            <div className="w-full p-2 md:w-1/2">
+              <p className="font-semibold">Email</p>
+              <p>{user?.email}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
