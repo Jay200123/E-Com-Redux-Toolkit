2d9b069868b78323995f7627571eea5848f20dfe
Jay200123 <kenken.ayonon@gmail.com>
2025-01-18 12:48:10 +0800

    Enhance Login page with image, password visibility toggle, and improved layout

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -2,10 +2,14 @@ import { useFormik } from "formik";
 import { useLoginMutation } from "../../state/api/reducer";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import ImageOne from "../../assets/login.jpg";
+import { useState } from "react";
 
 export default function () {
   const navigate = useNavigate();
   const [login] = useLoginMutation();
+
+  const [isShow, setIsShow] = useState(false);
 
   const formik = useFormik({
     initialValues: {
@@ -25,18 +29,40 @@ export default function () {
     },
   });
 
+  const tooglePassword = () => {
+    setIsShow(!isShow);
+  };
+
+  const signUp = () => {
+    navigate("/register");
+  }
+
   return (
     <form
-      className="flex justify-center w-full h-full"
+      className="flex justify-center transition-all duration-700 w-full h-[16rem] md:h-[32rem] p-3 bg-white md:p-1"
       onSubmit={formik.handleSubmit}
     >
-      <div className="flex flex-col items-center p-4 border border-black rounded-sm">
-        <div className="flex flex-col">
-          <label>Email</label>
+      <div className="hidden md:w-1/2 md:block">
+        <img
+          className="object-cover w-full h-full rounded-sm"
+          src={ImageOne}
+          alt="Image"
+        />
+      </div>
+      <div className="flex flex-col justify-center w-full p-4 rounded-md md:border md:border-gray-500 md:h-full md:w-1/2">
+        <h3 className="text-lg md:text-3xl">Sign In</h3>
+        <p className="mb-4 text-xs md:text-[1rem]">
+          Don't have an account yet?
+          <span onClick={signUp} className="ml-1 font-bold underline cursor-pointer">
+            Sign Up
+          </span>
+        </p>
+        <div className="flex flex-col mt-4">
+          <label className="text-sm font-medium md:text-lg">Email</label>
           <input
             name="email"
             id="email"
-            className="border border-black"
+            className="p-2 text-sm border border-black rounded-md"
             onBlur={formik.handleBlur}
             onChange={formik.handleChange}
             value={formik.values.email}
@@ -44,19 +70,33 @@ export default function () {
           />
         </div>
 
-        <div className="flex flex-col">
-          <label>Password</label>
+        <div className="flex flex-col mt-4">
+          <label className="text-sm font-medium md:text-lg">Password</label>
           <input
             name="password"
             id="password"
-            className="border border-black"
+            className="p-2 text-sm border border-black rounded-md"
             onBlur={formik.handleBlur}
             onChange={formik.handleChange}
             value={formik.values.password}
-            type="password"
+            type={isShow ? "text" : "password"}
           />
         </div>
-        <button type="submit">login</button>
+        <div className="flex items-center justify-between w-full mt-4">
+          <div className="flex items-center">
+            <input type="checkbox" onClick={tooglePassword} />
+            <label className="ml-1 text-xs md:text-sm">Show Password</label>
+          </div>
+          <div>
+            <a className="text-xs cursor-pointer md:text-sm">Forgot Password</a>
+          </div>
+        </div>
+        <button
+          className="text-[1rem] p-2 bg-black border transition-all duration-500 hover:opacity-75 rounded-md text-white mt-4"
+          type="submit"
+        >
+          login
+        </button>
       </div>
     </form>
   );
