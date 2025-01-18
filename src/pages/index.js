44b4f4e6bdeefdb35f10a1fce9e013e1312fcc8c
Jay200123 <kenken.ayonon@gmail.com>
2025-01-18 19:42:20 +0800

    Add Profile component to index export for routing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import Home from "./Home";
 import Login from "./Login";	 
 import Register from "./Register";
+import Profile from "./Profile";
 import ProtectedPage from "./ProtectedPage";   
 
 export {
      Home,
      Login,
      Register,
+     Profile,
      ProtectedPage,
 }
