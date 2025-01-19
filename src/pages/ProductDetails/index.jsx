bbd22be67039f22f6a8674e40347d70cf304f664
Jay200123 <kenken.ayonon@gmail.com>
2025-01-19 20:37:55 +0800

    Refactor ProductDetails layout for improved responsiveness and readability

--- src/pages/ProductDetails/index.jsx
+++ src/pages/ProductDetails/index.jsx
@@ -40,8 +40,8 @@ export default function () {
 
   return (
     <>
-      <div className="flex flex-col items-center w-full h-full overflow-x-auto md:flex-row">
-        <div className="relative w-1/2 h-full ">
+      <div className="flex flex-col items-center w-full max-h-[32rem] mt-3 overflow-x-auto md:flex-row">
+        <div className="relative w-full h-full md:w-1/2 ">
           <i
             onClick={back}
             className="absolute m-2 text-2xl cursor-pointer fa fa-arrow-left"
@@ -65,29 +65,29 @@ export default function () {
             />
           )}
         </div>
-        <div className="flex flex-col w-1/2 h-full p-2 justify-evenly">
-          <h3 className="text-2xl text-center">{product?.product_name}</h3>
-          <h3 className="text-lg">Product Description:</h3>
-          <p className="text-sm">{product?.description}</p>
-          <h3 className="text-lg">Unit Price:</h3>
-          <p className="text-sm">₱{product?.price}</p>
-          <h3 className="text-lg">Colors Available:</h3>
+        <div className="flex flex-col w-full h-full p-2 md:w-1/2 justify-evenly">
+          <h3 className="text-lg text-center md:text-2xl">{product?.product_name}</h3>
+          <h3 className="text-sm md:text-lg">Product Description:</h3>
+          <p className="text-xs md:text-sm">{product?.description}</p>
+          <h3 className="text-sm md:text-lg">Unit Price:</h3>
+          <p className="text-xs md:text-sm">₱{product?.price}</p>
+          <h3 className="text-sm md:text-lg">Colors Available:</h3>
           <div className="flex space-x-4">
             {productColors?.map((color, index) => (
               <div
                 key={`${color}-${index}`}
-                className="w-8 h-8 border border-black rounded-full"
+                className="w-6 h-6 border border-black rounded-full md:w-8 md:h-8"
                 style={{ backgroundColor: color }}
               ></div>
             ))}
           </div>
 
-          <h3 className="text-lg">Available Stocks:</h3>
-          <p className="text-sm">{product?.quantity} pcs</p>
+          <h3 className="text-sm md:text-lg">Available Stocks:</h3>
+          <p className="text-xs md:text-sm">{product?.quantity} pcs</p>
 
           <div className="flex flex-col items-center w-full p-4">
             <div className="flex items-center space-x-2">
-              <label className="text-lg font-bold">Qty:</label>
+              <label className="text-sm font-bold md:text-lg">Qty:</label>
               <div className="flex items-center border border-gray-300 rounded-md">
                 <button
                   className="px-3 py-1 text-lg text-gray-600 bg-gray-100 hover:bg-gray-200 rounded-l-md"
@@ -112,7 +112,7 @@ export default function () {
             {product && (
               <button
                 onClick={() => addCart(product, quantity)}
-                className="w-full p-2 mt-4 text-white transition-all duration-500 bg-black rounded-md hover:opacity-85"
+                className="w-full p-2 mt-4 text-sm text-white transition-all duration-500 bg-black rounded-md md:text-lg hover:opacity-85"
               >
                 Add to Cart
               </button>
