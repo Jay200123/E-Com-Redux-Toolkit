1a0597e3023355a074895a3db55dc99d9983ba6c
Jay200123 <kenken.ayonon@gmail.com>
2025-01-20 14:43:00 +0800

    Add toast notification for ProtectedRoutes component

--- src/components/ProtectedRoutes/index.jsx
+++ src/components/ProtectedRoutes/index.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 export default function({ children, userRole }) {
   const auth = useSelector((state) => state.auth);
