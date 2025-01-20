09a235fd7ba1ce21b40ff27763f9663b2ad4385c
Jay200123 <kenken.ayonon@gmail.com>
2025-01-20 14:43:03 +0800

    Add Checkout route with ProtectedRoutes for user access

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,64 @@
-  import {
-    Route,
-    createBrowserRouter,
-    createRoutesFromElements,
-    RouterProvider,
-  } from "react-router-dom";
-  import { ProtectedRoutes } from "./components";
-  import { MainLayout } from "./layout";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import { ProtectedRoutes } from "./components";
+import { MainLayout } from "./layout";
 
-  import { 
-    Home,
-    Login,
-    Register,
-    Profile,
-    Cart,
-    ProductDetails,
+import {
+  Home,
+  Login,
+  Register,
+  Profile,
+  Cart,
+  Checkout,
+  ProductDetails,
 } from "./pages";
 
-  function App() {
-    const Router = createBrowserRouter(
-      createRoutesFromElements(
-        <Route>
-          {/* Public Routes  */}
-          <Route element={<MainLayout />}>
-            <Route path="/" index element={<Home />} />
-            <Route path="/register" index element={<Register />} />
+function App() {
+  const Router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        {/* Public Routes  */}
+        <Route element={<MainLayout />}>
+          <Route path="/" index element={<Home />} />
+          <Route path="/register" index element={<Register />} />
 
-            <Route path="/login" index element={<Login />} />
-            <Route path="/cart" index element={<Cart />} />
-            <Route path="/product/:id" index element={<ProductDetails />} />  
+          <Route path="/login" index element={<Login />} />
+          <Route path="/cart" index element={<Cart />} />
+          <Route path="/product/:id" index element={<ProductDetails />} />
 
-            {/* Protected Routes */}
-            <Route
-              path="/profile"
-              index
-              element={
-                <ProtectedRoutes userRole={["User"]}>
-                  <Profile />
-                </ProtectedRoutes>
-              }
-            />
-          </Route>
+          {/* Protected Routes */}
+          <Route
+            path="/profile"
+            index
+            element={
+              <ProtectedRoutes userRole={["User"]}>
+                <Profile />
+              </ProtectedRoutes>
+            }
+          />
+
+          <Route
+            path="/checkout"
+            index
+            element={
+              <ProtectedRoutes userRole={["User"]}>
+                <Checkout />
+              </ProtectedRoutes>
+            }
+          />
         </Route>
-      )
-    );
-    return (
-      <>
-        <RouterProvider router={Router} />
-      </>
-    );
-  }
+      </Route>
+    )
+  );
+  return (
+    <>
+      <RouterProvider router={Router} />
+    </>
+  );
+}
 
-  export default App;
+export default App;
