a52f0d1c63bc4eda9eaef9697df2daeb06043bb7
Jay200123 <kenken.ayonon@gmail.com>
2025-01-21 17:08:24 +0800

    Add FilterReducer to rootReducer for filter state management

--- src/state/reducer.js
+++ src/state/reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from "@reduxjs/toolkit";
 import AuthReducer from "./slice/auth";
 import CartReducer from "./slice/cart";
 import CategoryReducer from "./slice/category";
+import FilterReducer from "./slice/filter";
 import { api } from "./api/reducer";
 
 export const rootReducer = combineReducers({
   auth: AuthReducer,
   cart: CartReducer,
   category: CategoryReducer,  
+  filter: FilterReducer,
   [api.reducerPath]: api.reducer,
 });
