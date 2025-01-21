598b512a0f150da070f6e8fc8c47caa4f56d5f3f
Jay200123 <kenken.ayonon@gmail.com>
2025-01-21 11:05:30 +0800

    Switch API_URL to local development server for testing purposes

--- src/constants/resource.js
+++ src/constants/resource.js
@@ -1,5 +1,5 @@
 export default {
-    // API_URL: "http://localhost:4000/api/v1",
-    API_URL: "https://rtk-backend-alpha.vercel.app/api/v1", 
+    API_URL: "http://localhost:4000/api/v1",
+    // API_URL: "https://rtk-backend-alpha.vercel.app/api/v1", 
     INCLUDE: "include",   
 }
