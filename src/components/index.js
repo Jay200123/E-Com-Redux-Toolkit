18fe54d3f5bb98cb60f991b0c8747a101fdb5ec9
Jay200123 <kenken.ayonon@gmail.com>
2025-01-21 11:05:23 +0800

    Add Sidebar component to index export for improved layout options

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,13 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Carousel from "./Carousel";
+import Sidebar from "./Sidebar";
 import ProtectedRoutes from "./ProtectedRoutes";
 
 export { 
     Navbar,
     Footer,
     Carousel,
+    Sidebar,
     ProtectedRoutes
 };
