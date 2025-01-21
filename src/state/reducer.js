7693693982c653addafc61a20186fed7982f29d6
Jay200123 <kenken.ayonon@gmail.com>
2025-01-21 14:56:47 +0800

    Add CategoryReducer to rootReducer for category state management

--- src/state/reducer.js
+++ src/state/reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import AuthReducer from "./slice/auth";
 import CartReducer from "./slice/cart";
+import CategoryReducer from "./slice/category";
 import { api } from "./api/reducer";
 
 export const rootReducer = combineReducers({
   auth: AuthReducer,
   cart: CartReducer,
+  category: CategoryReducer,  
   [api.reducerPath]: api.reducer,
 });
