781ec4a1c08ef752d4870856512230a7ef3178d2
Jay200123 <kenken.ayonon@gmail.com>
2025-01-21 14:56:30 +0800

    Adjust layout width in CategoryLayout for improved responsiveness

--- src/layout/CategoryLayout/index.jsx
+++ src/layout/CategoryLayout/index.jsx
@@ -7,7 +7,7 @@ export default function () {
       <div className="w-[40%] md:w-[20%] transition-all duration-500 ease-in-out">  
         <Sidebar />
       </div>
-      <div className="w-[70%] border-black transition-all duration-500 ease-in-out">
+      <div className="w-[60%] md:w-[80%] border-black transition-all duration-500 ease-in-out">
         <Outlet />
       </div>
     </main>
