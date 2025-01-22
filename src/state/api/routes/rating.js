d34eab350a04707c10ca2b8f069ca2ef8ccf84d8
Jay200123 <kenken.ayonon@gmail.com>
2025-01-22 20:49:11 +0800

    Add API routes for ratings management including CRUD operations

---
+++ src/state/api/routes/rating.js
@@ -0,0 +1,71 @@
+import { TAGS, API } from "../../../constants";
+
+export const getAll = (builder) => {
+  return builder.query({
+    query: () => {
+      return {
+        url: "/ratings",
+        method: API.GET,
+        providesTags: [TAGS.RATINGS],
+      };
+    },
+  });
+};
+
+export const getById = (builder) => {
+  return builder.query({
+    query: (id) => {
+      return {
+        url: `/rating/${id}`,
+        method: API.GET,
+        providesTags: [TAGS.RATINGS],
+      };
+    },
+  });
+};
+
+export const Add = (builder) => {
+  return builder.mutation({
+    query: (payload) => {
+      return {
+        url: `/ratings`,
+        method: API.POST,
+        body: payload,
+        invalidatesTags: [TAGS.RATINGS],
+      };
+    },
+  });
+};
+
+export const updateById = (builder) => {
+  return builder.mutation({
+    query: ({ id, payload }) => {
+      return {
+        url: `/rating/edit/${id}`,
+        method: API.PATCH,
+        body: payload,
+        invalidatesTags: [TAGS.RATINGS],
+      };
+    },
+  });
+};
+
+export const deleteById = (builder) => {
+  return builder.mutation({
+    query: (id) => {
+      return {
+        url: `/rating/${id}`,
+        method: API.DELETE,
+        invalidatesTags: [TAGS.RATINGS],
+      };
+    },
+  });
+};
+
+export default {
+  getAll,
+  getById,
+  Add,
+  updateById,
+  deleteById,
+};
