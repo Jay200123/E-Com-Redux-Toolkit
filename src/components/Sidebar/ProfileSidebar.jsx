2ace854720fdecf5199fda04bf522a1c3e2fb7eb
Jay200123 <kenken.ayonon@gmail.com>
2025-01-23 21:21:55 +0800

    Add navigation to Orders in ProfileSidebar with routing

--- src/components/Sidebar/ProfileSidebar.jsx
+++ src/components/Sidebar/ProfileSidebar.jsx
@@ -1,11 +1,18 @@
 import Logo from "../../assets/web-logo.png";
+import { useNavigate } from "react-router-dom";
 
 export default function () {
+  const navigate = useNavigate();
+
+  const handleOrders = () => {
+    navigate("/user/orders");
+  };
+
   return (
     <div className="flex flex-col items-center justify-between w-full h-full overflow-hidden">
-        <div className="flex justify-center w-full p-2 mt-2">
-            <img src={Logo} className="object-contain w-28 h-28 md:w-36 md:h-36" />
-        </div>
+      <div className="flex justify-center w-full p-2 mt-2">
+        <img src={Logo} className="object-contain w-28 h-28 md:w-36 md:h-36" />
+      </div>
       <ul className="w-full p-1">
         <li className="p-2 mt-2 text-sm transition-all duration-500 rounded-md cursor-pointer md:text-lg md:font-medium hover:bg-black hover:text-white">
           <i className="mr-1 fa-solid fa-user"></i>User Profile
@@ -13,7 +20,7 @@ export default function () {
         <li className="p-2 mt-2 text-sm transition-all duration-500 rounded-md cursor-pointer md:text-lg md:font-medium hover:bg-black hover:text-white">
           <i className="mr-1 fa-solid fa-pencil-alt"></i>Edit Profile
         </li>
-        <li className="p-2 mt-2 text-sm transition-all duration-500 rounded-md cursor-pointer md:text-lg md:font-medium hover:bg-black hover:text-white">
+        <li onClick={handleOrders} className="p-2 mt-2 text-sm transition-all duration-500 rounded-md cursor-pointer md:text-lg md:font-medium hover:bg-black hover:text-white">
           <i className="mr-1 fa-solid fa-box"></i> Orders
         </li>
         <li className="p-2 mt-2 text-sm transition-all duration-500 rounded-md cursor-pointer md:text-lg md:font-medium hover:bg-black hover:text-white">
