47476ec491812982db02d250b0a30858c0c9eb29
Jay200123 <kenken.ayonon@gmail.com>
2025-01-23 16:59:05 +0800

    Update Edit Profile button visibility for improved mobile responsiveness

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -16,7 +16,7 @@ export default function () {
               alt={randomImage.originalname}   
               className="object-contain border border-gray-500 rounded-full w-36 h-36 md:w-60 md:h-60"
             />
-            <button onClick={()=>navigate(`/profile/edit`)} className="w-full p-2 mt-2 text-sm text-white bg-black rounded-md md:mt-4 md:text-lg">Edit Profile</button>
+            <button onClick={()=>navigate(`/profile/edit`)} className="block w-full p-2 mt-2 text-sm text-white bg-black rounded-md md:hidden md:mt-4 md:text-lg">Edit Profile</button>
           </div>
         </div>
         <div className="flex flex-col w-full h-full md:w-[70%] p-2">
