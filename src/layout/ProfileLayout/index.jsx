5449450373fe09f29bd8a7f1e3fddfd3f7cead19
Jay200123 <kenken.ayonon@gmail.com>
2025-01-23 21:21:59 +0800

    Adjust ProfileSidebar width for improved layout responsiveness

--- src/layout/ProfileLayout/index.jsx
+++ src/layout/ProfileLayout/index.jsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 export default function () {
   return (
     <main className="flex min-w-full min-h-screen md:flex-row">
-      <div className="md:w-[30%] hidden md:block transition-all duration-500 ease-in-out">  
+      <div className="md:w-[20%] hidden md:block transition-all duration-500 ease-in-out">  
         <ProfileSidebar />
       </div>
       <div className="w-full md:w-[80%] border-black transition-all duration-500 ease-in-out">
