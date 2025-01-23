8e4eb383de8ddca5cc7338e574bbef691de72765
Jay200123 <kenken.ayonon@gmail.com>
2025-01-23 16:59:00 +0800

    Refactor ProfileLayout component for improved sidebar visibility and layout responsiveness

--- src/layout/ProfileLayout/index.jsx
+++ src/layout/ProfileLayout/index.jsx
@@ -4,10 +4,10 @@ import { Outlet } from "react-router-dom";
 export default function () {
   return (
     <main className="flex min-w-full min-h-screen md:flex-row">
-      <div className="w-[40%] md:w-[30%] transition-all duration-500 ease-in-out">  
+      <div className="md:w-[30%] hidden md:block transition-all duration-500 ease-in-out">  
         <ProfileSidebar />
       </div>
-      <div className="w-[60%] md:w-[80%] border-black transition-all duration-500 ease-in-out">
+      <div className="w-full md:w-[80%] border-black transition-all duration-500 ease-in-out">
         <Outlet />
       </div>
     </main>
