f9f53bd3dd6431a95bc033ebf9c03b54c7454720
Jay200123 <kenken.ayonon@gmail.com>
2025-01-24 22:05:07 +0800

    Add BrandCategory to exports in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,31 @@
 import Home from "./Home";
-import Login from "./Login";	 
+import Login from "./Login";
 import Register from "./Register";
 import Profile from "./Profile";
-import Cart from "./Cart";   
-import ProductDetails from "./ProductDetails"; 
-import Checkout from "./Cart/checkout"; 
+import Cart from "./Cart";
+import ProductDetails from "./ProductDetails";
+import Checkout from "./Cart/checkout";
 import EditProfile from "./Profile/editProfile";
-import ProductCategory from "./ProductCategory"
+import ProductCategory from "./ProductCategory";
 import Orders from "./Orders";
 import OrderDetails from "./Orders/orderDetails";
 import Ratings from "./Ratings";
-import ProtectedPage from "./ProtectedPage";   
+import BrandCategory from "./BrandCategory";
+import ProtectedPage from "./ProtectedPage";
 
 export {
-     Home,
-     Login,
-     Register,
-     Profile,
-     Cart,
-     ProductDetails,
-     Checkout,
-     EditProfile,
-     ProductCategory,
-     Orders,
-     OrderDetails,
-     Ratings,
-     ProtectedPage,
-}
+  Home,
+  Login,
+  Register,
+  Profile,
+  Cart,
+  ProductDetails,
+  Checkout,
+  EditProfile,
+  ProductCategory,
+  Orders,
+  OrderDetails,
+  Ratings,
+  BrandCategory,
+  ProtectedPage,
+};
