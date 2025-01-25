7bdeb57f419c89a03d8f3d9fa045263fbac9bdb1
Jay200123 <kenken.ayonon@gmail.com>
2025-01-25 15:09:45 +0800

    Add menu navigation to Navbar component for improved accessibility

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -25,10 +25,13 @@ export default function () {
     navigate("/user/orders");
   };
 
-  const handleShop = () => { 
+  const handleShop = () => {
     navigate("/shop");
-  }
+  };
 
+  const handleMenu = () => {
+    navigate("/menu");
+  };
 
   const handleLogout = () => {
     dispatch(logout());
@@ -47,61 +50,97 @@ export default function () {
       >
         <i className="mr-1 fa-solid fa-screwdriver-wrench"></i>Tech Fix
       </h3>
-      <ul className="flex flex-row items-center overflow-x-auto">
-        <li onClick={handleShop} className="mr-1 text-xs font-medium cursor-pointer md:font-bold md:mr-2 md:text-sm">
-          Shop
-        </li>
-        {auth?.isAuthenticated ? (
-          <>
-            <li onClick={handleOrders} className="block mr-1 text-xs font-medium md:hidden md:font-bold md:mr-2 md:text-sm">
-              Orders
-            </li>
-            <li className="block mr-1 text-xs font-medium md:hidden md:font-bold md:mr-2 md:text-sm">
-              Reviews
-            </li>
-          </>
-        ) : (
-          <></>
-        )}
+      <div className="hidden md:block">
+        <ul className="flex flex-row items-center overflow-x-auto">
+          <li
+            onClick={handleShop}
+            className="mr-1 text-xs font-medium cursor-pointer md:font-bold md:mr-2 md:text-sm"
+          >
+            <i className="mr-[2px] fa-solid fa-bag-shopping"></i> Shop
+          </li>
+          {auth?.isAuthenticated ? (
+            <>
+              <li
+                onClick={handleOrders}
+                className="block mr-1 text-xs font-medium md:hidden md:font-bold md:mr-2 md:text-sm"
+              >
+                Orders
+              </li>
+              <li className="block mr-1 text-xs font-medium md:hidden md:font-bold md:mr-2 md:text-sm">
+                Reviews
+              </li>
+            </>
+          ) : (
+            <></>
+          )}
 
-        <li className="mr-1 text-xs font-medium md:font-bold md:mr-2 md:text-sm">
-          About
-        </li>
-        <li className="mr-1 text-xs font-medium truncate md:font-bold md:mr-2 md:text-sm">
-          Contact Us
-        </li>
-      </ul>
-      <ul className="flex flex-row items-center">
-        {auth?.isAuthenticated ? (
-          <>
-            <li
-              onClick={myCart}
-              className="relative p-2 mr-4 text-lg font-medium md:font-bold"
-            >
-              <span className="absolute top-0 right-0 flex items-center justify-center w-5 h-5 p-1 text-xs font-medium text-white bg-red-500 rounded-full cursor-pointer md:text-sm">
-                {cart?.item?.length || 0}
-              </span>
-              <i className="fa-solid fa-cart-shopping"></i>
-            </li>
-            <li
-              onClick={() => navigate("/profile")}
-              className="space-x-1 text-xs font-medium cursor-pointer md:font-bold md:text-sm"
-            >
-              <img
-                src={randomImage.url}
-                alt="avatar"
-                className="w-8 h-8 rounded-full md:w-10 md:h-10"
-              />
-            </li>
-            <li
-              onClick={handleLogout}
-              className="p-1 ml-1 font-medium cursor-pointer md:p-2 md:font-bold"
-            >
-              <i className="text-lg fa-solid fa-right-from-bracket"></i>
-            </li>
-          </>
-        ) : (
-          <>
+          <li className="mr-1 text-xs font-medium md:font-bold md:mr-2 md:text-sm">
+            <i className="mr-1 fa-solid fa-circle-info"></i> About
+          </li>
+          <li className="mr-1 text-xs font-medium truncate md:font-bold md:mr-2 md:text-sm">
+            <i className="mr-1 fa-solid fa-phone"></i>Contact Us
+          </li>
+        </ul>
+      </div>
+      <div className="hidden md:block">
+        <ul className="flex flex-row items-center">
+          {auth?.isAuthenticated ? (
+            <>
+              <li
+                onClick={myCart}
+                className="relative p-2 mr-4 text-lg font-medium md:font-bold"
+              >
+                <span className="absolute top-0 right-0 flex items-center justify-center w-5 h-5 p-1 text-xs font-medium text-white bg-red-500 rounded-full cursor-pointer md:text-sm">
+                  {cart?.item?.length || 0}
+                </span>
+                <i className="fa-solid fa-cart-shopping"></i>
+              </li>
+              <li
+                onClick={() => navigate("/profile")}
+                className="space-x-1 text-xs font-medium cursor-pointer md:font-bold md:text-sm"
+              >
+                <img
+                  src={randomImage.url}
+                  alt="avatar"
+                  className="w-8 h-8 rounded-full md:w-10 md:h-10"
+                />
+              </li>
+              <li
+                onClick={handleLogout}
+                className="p-1 ml-1 font-medium cursor-pointer md:p-2 md:font-bold"
+              >
+                <i className="text-lg fa-solid fa-right-from-bracket"></i>
+              </li>
+            </>
+          ) : (
+            <>
+              <li
+                onClick={myCart}
+                className="relative p-2 mr-4 text-lg font-medium md:font-bold"
+              >
+                <span className="absolute top-0 right-0 flex items-center justify-center w-5 h-5 p-1 text-sm font-medium text-white bg-red-500 rounded-full cursor-pointer">
+                  {cart?.item?.length || 0}
+                </span>
+                <i className="fa-solid fa-cart-shopping"></i>
+              </li>
+              <li
+                onClick={login}
+                className="mr-1 text-xs font-medium cursor-pointer md:mr-2 md:font-bold md:text-sm"
+              >
+                <i className="fa-solid fa-right-to-bracket mr-[2px]"></i>LOGIN
+              </li>
+              <li
+                onClick={register}
+                className="mr-1 text-xs font-medium truncate cursor-pointer md:mr-2 md:font-bold md:text-sm"
+              >
+                <i className="fa-solid fa-user-plus mr-[2px]"></i> SIGN UP
+              </li>
+            </>
+          )}
+        </ul>
+      </div>
+      <div className="block p-1 mr-4 rounded-md md:hidden ">
+        <ul className="flex flex-row items-center">
             <li
               onClick={myCart}
               className="relative p-2 mr-4 text-lg font-medium md:font-bold"
@@ -111,21 +150,14 @@ export default function () {
               </span>
               <i className="fa-solid fa-cart-shopping"></i>
             </li>
-            <li
-              onClick={login}
-              className="mr-1 text-xs font-medium cursor-pointer md:mr-2 md:font-bold md:text-sm"
-            >
-              LOGIN
-            </li>
-            <li
-              onClick={register}
-              className="mr-1 text-xs font-medium truncate cursor-pointer md:mr-2 md:font-bold md:text-sm"
-            >
-              SIGN UP
-            </li>
-          </>
-        )}
-      </ul>
+          <li
+            className="p-1 transition-all duration-500 rounded-md hover:bg-gray-500 hover:text-white "
+            onClick={handleMenu}
+          >
+            <i className="2xl fa-solid fa-bars"></i>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
