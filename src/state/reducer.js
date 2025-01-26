4fc243f0d843d8e9ca8f1c8814b6eab776c8d234
Jay200123 <kenken.ayonon@gmail.com>
2025-01-26 14:09:43 +0800

    Add RatingReducer to rootReducer for product rating management

--- src/state/reducer.js
+++ src/state/reducer.js
@@ -3,12 +3,14 @@ import AuthReducer from "./slice/auth";
 import CartReducer from "./slice/cart";
 import CategoryReducer from "./slice/category";
 import FilterReducer from "./slice/filter";
+import RatingReducer from "./slice/rating";
 import { api } from "./api/reducer";
 
 export const rootReducer = combineReducers({
   auth: AuthReducer,
   cart: CartReducer,
-  category: CategoryReducer,  
+  category: CategoryReducer,
   filter: FilterReducer,
+  rating: RatingReducer,
   [api.reducerPath]: api.reducer,
 });
