6d5a1ad7ca936166a9f4831c8236215fc60646e9
Jay200123 <kenken.ayonon@gmail.com>
2025-01-27 20:42:26 +0800

    Add AdminSidebar component to index export

--- src/components/index.js
+++ src/components/index.js
@@ -3,13 +3,15 @@ import Footer from "./Footer";
 import Carousel from "./Carousel";
 import Sidebar from "./Sidebar";
 import ProfileSidebar from "./Sidebar/ProfileSidebar";
+import AdminSidebar from "./Sidebar/AdminSidebar";
 import ProtectedRoutes from "./ProtectedRoutes";
 
-export { 
-    Navbar,
-    Footer,
-    Carousel,
-    Sidebar,
-    ProfileSidebar,
-    ProtectedRoutes
+export {
+  Navbar,
+  Footer,
+  Carousel,
+  Sidebar,
+  ProfileSidebar,
+  AdminSidebar,
+  ProtectedRoutes,
 };
