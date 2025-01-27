9f7f435f5129df26b025f5e1e080b3d3a1d686f3
Jay200123 <kenken.ayonon@gmail.com>
2025-01-27 20:42:30 +0800

    Enhance ProtectedRoutes component to redirect users based on their roles

--- src/components/ProtectedRoutes/index.jsx
+++ src/components/ProtectedRoutes/index.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
-export default function({ children, userRole }) {
+export default function ({ children, userRole }) {
   const auth = useSelector((state) => state.auth);
 
   if (!auth?.isAuthenticated) {
@@ -15,9 +15,15 @@ export default function({ children, userRole }) {
       : auth?.user?.role === userRole;
 
     if (!isRoleAuthorized) {
-      return <Navigate to="/unauthorized" />;
+      if(auth?.user?.role === "User"){
+        return <Navigate to="/profile" replace />;  
+      } else if (auth?.user?.role === "Admin"){
+        return <Navigate to="/admin/dashboard" replace />;  
+      }else {
+        return <Navigate to="/login" replace />;
+      }
     }
   }
 
   return <>{children}</>;
-};
+}
