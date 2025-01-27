fa4c43c5fc28228288e332b268d4176175691af9
Jay200123 <kenken.ayonon@gmail.com>
2025-01-27 20:42:37 +0800

    Add AdminLayout to index export for improved layout management

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,9 +1,11 @@
 import MainLayout from "./MainLayout";
 import CategoryLayout from "./CategoryLayout";
 import ProfileLayout from "./ProfileLayout";    
+import AdminLayout from "./AdminLayout";
 
 export {
     MainLayout,
     CategoryLayout,
-    ProfileLayout
+    ProfileLayout,
+    AdminLayout,
 }
