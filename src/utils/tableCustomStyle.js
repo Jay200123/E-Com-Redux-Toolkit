72f9aad9e16ad2fecb1b1e1cd2d0b2c99ffc16e5
Jay200123 <kenken.ayonon@gmail.com>
2025-01-28 13:17:28 +0800

    Add custom styles for table components to enhance UI appearance

---
+++ src/utils/tableCustomStyle.js
@@ -0,0 +1,23 @@
+const tableCustomStyles = {
+  headCells: {
+    style: {
+      backgroundColor: "#374151",
+      fontSize: "15px",
+      color: "#ffffff",
+    },
+  },
+  cells: {
+    style: {
+      backgroundColor: "#f2f2f2",
+      fontSize: "14px",
+      borderBottom: "1px solid #b3b3b3",
+    },
+    table: {
+      style: {
+        backgroundColor: "transparent",
+      },
+    },
+  },
+};
+
+export { tableCustomStyles };
