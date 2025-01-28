019c8933be3d2b5116957e0347bfbe37242a2539
Jay200123 <kenken.ayonon@gmail.com>
2025-01-28 15:00:37 +0800

    Add MainAdminLayout to index export for improved admin routing

--- src/layout/index.js
+++ src/layout/index.js
@@ -2,10 +2,12 @@ import MainLayout from "./MainLayout";
 import CategoryLayout from "./CategoryLayout";
 import ProfileLayout from "./ProfileLayout";    
 import AdminLayout from "./AdminLayout";
+import MainAdminLayout from "./MainAdminLayout";
 
 export {
     MainLayout,
     CategoryLayout,
     ProfileLayout,
     AdminLayout,
+    MainAdminLayout,
 }
