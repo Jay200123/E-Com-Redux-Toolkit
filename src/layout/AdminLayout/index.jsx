9460a58dc786b162dbbe59d51fa693f963ab0e42
Jay200123 <kenken.ayonon@gmail.com>
2025-01-28 13:17:10 +0800

    Refactor AdminLayout to remove unnecessary border styling for improved UI consistency

--- src/layout/AdminLayout/index.jsx
+++ src/layout/AdminLayout/index.jsx
@@ -4,10 +4,10 @@ import { AdminSidebar } from "../../components";
 export default function () {
   return (
     <main className="flex min-h-screen transition-all duration-500 min-w-screen">
-      <div className="md:w-[20%] hidden border border-black md:block transition-all duration-500 ease-in-out">
+      <div className="md:w-[20%] hidden md:block transition-all duration-500 ease-in-out">
         <AdminSidebar />
       </div>
-      <div className="w-full md:w-[80%] border border-black transition-all duration-500 ease-in-out">
+      <div className="w-full md:w-[80%] transition-all duration-500 ease-in-out">
         <Outlet />
       </div>
     </main>
