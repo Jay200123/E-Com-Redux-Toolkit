79deb288b7f3afe16f6175b0775d75ead8f9a231
Jay200123 <kenken.ayonon@gmail.com>
2025-01-28 21:50:17 +0800

    Update table font sizes for improved readability

--- src/utils/tableCustomStyle.js
+++ src/utils/tableCustomStyle.js
@@ -2,14 +2,14 @@ const tableCustomStyles = {
   headCells: {
     style: {
       backgroundColor: "#374151",
-      fontSize: "14px",
+      fontSize: "16px",
       color: "#ffffff",
     },
   },
   cells: {
     style: {
       backgroundColor: "#f2f2f2",
-      fontSize: "12px",
+      fontSize: "14px",
       borderBottom: "1px solid #b3b3b3",
     },
     table: {
