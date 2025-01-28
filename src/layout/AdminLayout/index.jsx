5a62a1b5168265f16c94bf77e582139c13dc6308
Jay200123 <kenken.ayonon@gmail.com>
2025-01-28 15:00:41 +0800

    Add border to AdminSidebar for improved visual separation

--- src/layout/AdminLayout/index.jsx
+++ src/layout/AdminLayout/index.jsx
@@ -4,7 +4,7 @@ import { AdminSidebar } from "../../components";
 export default function () {
   return (
     <main className="flex min-h-screen transition-all duration-500 min-w-screen">
-      <div className="md:w-[20%] hidden md:block transition-all duration-500 ease-in-out">
+      <div className="md:w-[20%] border border-gray-500 hidden md:block transition-all duration-500 ease-in-out">
         <AdminSidebar />
       </div>
       <div className="w-full md:w-[80%] transition-all duration-500 ease-in-out">
