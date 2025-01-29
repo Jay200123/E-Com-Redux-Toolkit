517026217c11b461442fc72d1f9a6fb1b5d6c1ec
Jay200123 <kenken.ayonon@gmail.com>
2025-01-29 12:05:16 +0800

    Refactor BrandCategory component layout for improved image display and alignment

--- src/pages/BrandCategory/index.jsx
+++ src/pages/BrandCategory/index.jsx
@@ -131,25 +131,28 @@ export default function () {
           filteredProducts.map((p) => (
             <div
               key={p?._id}
-              className="flex mt-2 flex-col  border border-gray-500 rounded-md h-[16rem] md:h-[18rem] overflow-hidden p-2"
+              className="flex mt-2 flex-col  border border-gray-500 rounded-md h-[18rem] md:h-[19rem] overflow-hidden p-2"
             >
-              {p?.image?.length > 1 ? (
-                <img
-                  src={
-                    p?.image[Math.floor(Math.random() * p?.image.length)]?.url
-                  }
-                  alt={p?.product_name || "Product Image"}
-                  onClick={() => navigate(`/product/${p?._id}`)}
-                  className="object-contain w-40 h-40 cursor-pointer"
-                />
-              ) : (
-                <img
-                  src={p?.image[0]?.url}
-                  alt={p?.name || "Product Image"}
-                  onClick={() => navigate(`/product/${p?._id}`)}
-                  className="object-contain w-40 h-40 cursor-pointer"
-                />
-              )}
+              <div className="flex justify-center">
+                {p?.image?.length > 1 ? (
+                  <img
+                    src={
+                      p?.image[Math.floor(Math.random() * p?.image.length)]?.url
+                    }
+                    alt={p?.product_name || "Product Image"}
+                    onClick={() => navigate(`/product/${p?._id}`)}
+                    className="object-contain w-40 h-40 cursor-pointer"
+                  />
+                ) : (
+                  <img
+                    src={p?.image[0]?.url}
+                    alt={p?.name || "Product Image"}
+                    onClick={() => navigate(`/product/${p?._id}`)}
+                    className="object-contain w-40 h-40 cursor-pointer"
+                  />
+                )}
+              </div>
+
               <p className="text-sm truncate md:text-sm text-medium">
                 {p?.product_name || "Unnamed Product"}
               </p>
@@ -177,7 +180,7 @@ export default function () {
                     toast.success("Product added to cart");
                   }}
                   className="text-lg cursor-pointer md:text-2xl"
-                />  
+                />
               </div>
             </div>
           ))
