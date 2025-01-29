43ec8ae3a1b6523e9d86c6361e1211ee0d3f25ca
Jay200123 <kenken.ayonon@gmail.com>
2025-01-29 12:05:28 +0800

    Add OrderTable component for displaying and managing orders

---
+++ src/pages/OrderTable/index.jsx
@@ -0,0 +1,120 @@
+import { useGetOrdersQuery } from "../../state/api/reducer";
+import DataTable from "react-data-table-component";
+import { FadeLoader } from "react-spinners";
+import { tableCustomStyles } from "../../utils/tableCustomStyle";
+import { useNavigate } from "react-router-dom";
+import { FaBox, FaShip, FaTruckMoving, FaTrash, FaEye } from "react-icons/fa";
+import { useState } from "react";
+
+export default function () {
+  const navigate = useNavigate();
+  const { data, isLoading } = useGetOrdersQuery();
+  const orders = data?.details || [];
+
+  const [order, setOrder] = useState("");
+
+  const filteredOrders = orders?.filter((o) => o?.orderNumber?.toLowerCase()?.includes(order?.toLowerCase()));  
+
+  const columns = [
+    {
+      name: "OR Number",
+      selector: (row) => row?.orderNumber,
+      sortable: true,
+    },
+    {
+      name: "User",
+      selector: (row) => row?.user?.fullname,
+      sortable: true,
+    },
+    {
+      name: "Ordered Items",
+      cell: (row) => {
+        return (
+          <ul className="space-y-1 text-sm list-disc list-inside">
+            {row?.products?.map((p, index) => (
+              <li className="truncate" key={index}>
+                {p?.product?.product_name}{" "}
+                <span className="font-medium">x {p?.quantity}</span>
+              </li>
+            ))}
+          </ul>
+        );
+      },
+    },
+    {
+      name: "Status",
+      selector: (row) => row?.status,
+      sortable: true,
+    },
+    {
+      name: "Payment",
+      selector: (row) => row?.payment,
+      sortable: true,
+    },
+    {
+      name: "Date Placed",
+      selector: (row) => new Date(row?.date_placed.toLocaleString()).toISOString().split("T")[0],    
+      sortable: true,
+    },
+    {
+      name: "Date delivered",
+      selector: (row) =>
+        row?.date_delivered ? new Date(row?.date_delivered.toLocaleString()).toISOString().split("T")[0] : "Not yet delivered",
+      sortable: true,
+    },
+    {
+      name: "Actions",
+      cell: (row) => (
+        <div className="flex items-center text-center">
+          <FaBox
+            title="Packed Order"
+            className="mr-2 text-xl text-yellow-500"
+          />
+          <FaShip
+            title="Shipped Order"
+            className="mr-2 text-xl text-blue-500"
+          />
+          <FaTruckMoving
+            title="Deliver Order"
+            className="mr-1 text-xl text-green-500"
+          />
+          <FaEye title="View Order" className="mr-1 text-xl text-gray-500" />
+
+          <FaTrash title="Delete Order" className="mr-1 text-xl text-red-500" />
+        </div>
+      ),
+    },
+  ];
+
+  return (
+    <div className="w-full overflow-x-auto">
+      {isLoading ? (
+        <div className="flex items-center justify-center">
+          <FadeLoader color="#10B981" loading={isLoading} size={15} />
+        </div>
+      ) : (
+        <div className="max-w-3xl m-4 rounded-md shadow-md md:max-w-5xl">
+          <div className="w-full m-2">
+            <input
+              type="text"
+              placeholder="Enter OR Number"
+              onChange={(e) => setOrder(e.target.value)}
+              className="p-2 text-sm border border-black rounded-md"
+            />
+          </div>
+          <DataTable
+            title="Orders"
+            columns={columns}
+            data={filteredOrders || []} 
+            pagination
+            highlightOnHover
+            pointerOnHover
+            paginationPerPage={10}
+            paginationRowsPerPageOptions={[10, 20, 30]}
+            customStyles={tableCustomStyles}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
