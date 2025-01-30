7d81ac5842e13fb0df9b07f67ac87e38322d795f
Jay200123 <kenken.ayonon@gmail.com>
2025-01-30 20:32:50 +0800

    Add RatingTable import to index.js for improved component accessibility

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,6 +28,7 @@ import EditProduct from "./ProductTable/editProduct";
 import GetProductById from "./ProductTable/getProductById";
 import OrderTable from "./OrderTable";
 import GetOrderById from "./OrderTable/getOrderById";
+import RatingTable from "./RatingTable";
 import ProtectedPage from "./ProtectedPage";
 
 export {
@@ -61,5 +62,6 @@ export {
   GetProductById,
   OrderTable,
   GetOrderById,
+  RatingTable,
   ProtectedPage,
 };
