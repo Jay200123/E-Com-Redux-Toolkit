9b619a248b0f195280a8314ca3d95196f31a9747
Jay200123 <kenken.ayonon@gmail.com>
2025-01-30 18:22:56 +0800

    Add mutations for updating order status to packed, shipped, and delivered

--- src/state/api/routes/order.js
+++ src/state/api/routes/order.js
@@ -62,10 +62,49 @@ export const deleteById = (builder) => {
   });
 };
 
+export const packedById = (builder)=>{
+  return builder.mutation({
+    query: (id)=>{
+      return {
+        url: `/order/packed/${id}`,
+        method: API.PATCH,
+        invalidatesTags: [TAGS.ORDERS],
+      }
+    }
+  })
+}
+
+export const shippedById = (builder)=>{
+  return builder.mutation({
+    query: (id)=>{
+      return {
+        url: `/order/shipped/${id}`,
+        method: API.PATCH,
+        invalidatesTags: [TAGS.ORDERS],
+      }
+    }
+  })
+}
+
+export const deliveryById = (builder)=>{
+  return builder.mutation({
+    query: (id)=>{
+      return {
+        url: `/order/delivered/${id}`,
+        method: API.PATCH,
+        invalidatesTags: [TAGS.ORDERS],
+      }
+    }
+  })
+}
+
 export default {
   getAll,
   getById,
   Add,
   updateById,
   deleteById,
+  packedById, 
+  shippedById,
+  deliveryById
 };
