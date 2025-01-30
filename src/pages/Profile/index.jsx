5b15926c032a6408f4f4020c373805ecb339a7fd
Jay200123 <kenken.ayonon@gmail.com>
2025-01-30 20:39:57 +0800

    Refactor Profile component to conditionally navigate to edit profile based on user role

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -3,9 +3,10 @@ import { useNavigate } from "react-router-dom";
 
 export default function () {
   const navigate = useNavigate();
-  const auth = useSelector((state) => state.auth);  
-  const randomImage = auth?.user?.image[Math.floor(Math.random() * auth?.user.image.length)];
-    
+  const auth = useSelector((state) => state.auth);
+  const randomImage =
+    auth?.user?.image[Math.floor(Math.random() * auth?.user.image.length)];
+
   return (
     <div className="flex items-center justify-center w-full h-full p-1 overflow-hidden md:p-4">
       <div className="flex flex-col transition-all duration-700 md:flex-row w-[75rem] border border-gray-500 shadow-lg rounded-lg h-[48rem] md:h-[24rem]">
@@ -13,10 +14,19 @@ export default function () {
           <div className="flex flex-col items-center justify-center w-full h-full p-4">
             <img
               src={randomImage.url}
-              alt={randomImage.originalname}   
+              alt={randomImage.originalname}
               className="object-contain border border-gray-500 rounded-full w-36 h-36 md:w-60 md:h-60"
             />
-            <button onClick={()=>navigate(`/profile/edit`)} className="block w-full p-2 mt-2 text-sm text-white bg-black rounded-md md:hidden md:mt-4 md:text-lg">Edit Profile</button>
+            <button
+              onClick={() =>
+                auth?.role === "User"
+                  ? navigate(`/profile/edit`)
+                  : navigate(`/admin/profile/edit`)
+              }
+              className="block w-full p-2 mt-2 text-sm text-white bg-black rounded-md md:hidden md:mt-4 md:text-lg"
+            >
+              Edit Profile
+            </button>
           </div>
         </div>
         <div className="flex flex-col w-full h-full md:w-[70%] p-2">
