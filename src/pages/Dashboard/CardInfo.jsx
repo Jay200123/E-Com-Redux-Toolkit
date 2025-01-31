37b856d79b6a7e6d0e90145f73d318bc864e135e
Jay200123 <kenken.ayonon@gmail.com>
2025-01-31 23:40:10 +0800

    Add CardInfo component to display user, product, order counts and total revenue

---
+++ src/pages/Dashboard/CardInfo.jsx
@@ -0,0 +1,61 @@
+import {
+  useGetUsersQuery,
+  useGetProductsQuery,
+  useGetOrdersQuery,
+} from "../../state/api/reducer";
+import {
+  FaUser,
+  FaBoxOpen,
+  FaClipboardList,
+  FaDollarSign,
+} from "react-icons/fa";
+
+export default function () {
+  const { data: userDetails } = useGetUsersQuery();
+  const users = userDetails?.details;
+  const { data: productDetails } = useGetProductsQuery();
+  const products = productDetails?.details;
+  const { data: orderDetails } = useGetOrdersQuery();
+  const orders = orderDetails?.details;
+
+  const revenue = orders?.reduce((acc, order) => {
+    return acc?.price + order.price;
+  });
+
+  return (
+    <div className="flex w-full p-2 mb-4shadow-md justify-evenly md:flex-row md:justify-between">
+      <div className="flex flex-col overflow-hidden text-center border-r pl-1 border-gray-500  w-[25%] h-28 md:h-36">
+        <h3 className="text-xs font-bold text-center md:font-medium md:text-lg">
+          {" "}
+          Number of Users
+        </h3>
+        <p className="text-xs text-center md:text-2xl">{users?.length}</p>
+        <FaUser className="text-2xl text-green-500 md:text-4xl" />
+      </div>
+      <div className="flex flex-col overflow-hidden text-center border-r pl-1 border-gray-500    w-[25%]  h-28 md:h-36">
+        <h3 className="text-xs font-bold text-center md:font-medium md:text-lg">
+          {" "}
+          Number of Products
+        </h3>
+        <p className="text-xs text-center md:text-2xl">{products?.length}</p>
+        <FaBoxOpen className="text-2xl text-blue-500 md:text-4xl" />
+      </div>
+      <div className="flex flex-col w-[25%] overflow-hidden text-center border-r pl-1 border-gray-500  h-28 md:h-36">
+        <h3 className="text-xs font-bold text-center md:font-medium md:text-lg">
+          {" "}
+          Number of Orders
+        </h3>
+        <p className="text-xs text-center md:text-2xl">{orders?.length}</p>
+        <FaClipboardList className="text-2xl md:text-4xl text-[#FD7E14]" />
+      </div>
+      <div className="flex flex-col w-[25%] overflow-hidden text-center border-r pl-1 border-gray-500 h-28 md:h-36">
+        <h3 className="text-xs font-bold text-center md:font-medium md:text-lg">
+          {" "}
+          Total Revenue
+        </h3>
+        <p className="text-sm text-center md:text-2xl">{revenue}</p>
+        <FaDollarSign className="text-2xl text-black md:text-4xl" />
+      </div>
+    </div>
+  );
+}
