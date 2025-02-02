bc270c32e88e921b98be13145af70f3b0f082fc9
Jay200123 <kenken.ayonon@gmail.com>
2025-02-02 20:30:22 +0800

    Add validation schemas for editing and creating ratings, and update exports in validation index

--- src/validations/index.jsx
+++ src/validations/index.jsx
@@ -1,11 +1,21 @@
 import SignInValidationSchema from "./signInValidation";
 import SignUpValidationSchema from "./signUpValidation";
+import editProfileValidationSchema from "./editProfileValidation";  
 import createBrandValidationSchema from "./createBrandValidation";
-import editBrandValidationSchema from "./editBrandValidation";  
-import createProductValidationSchema from "./createProductValidation";  
-import editProductValidationSchema from "./editProductValidation";  
+import editBrandValidationSchema from "./editBrandValidation";
+import createProductValidationSchema from "./createProductValidation";
+import editProductValidationSchema from "./editProductValidation";
+import createRatingValidationSchema from "./createRatingValidation";
+import editRatingValidationSchema from "./editRatingValidation";
 
 export {
-    SignInValidationSchema,
-    SignUpValidationSchema,
-}
+  SignInValidationSchema,
+  SignUpValidationSchema,
+  editProfileValidationSchema,
+  createBrandValidationSchema,
+  editBrandValidationSchema,
+  createProductValidationSchema,
+  editProductValidationSchema,
+  createRatingValidationSchema,
+  editRatingValidationSchema,
+};
