2a25cae7ab918c53585a32ec9b02ee1af70a1e5a
Jay200123 <kenken.ayonon@gmail.com>
2025-02-02 14:26:43 +0800

    Add BrandInfo and PaymentInfo components to Dashboard layout

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,17 +1,21 @@
 import CardInfo from "./CardInfo";
 import OrderInfo from "./OrderInfo";
-import ProductInfo from "./ProductInfo";  
+import ProductInfo from "./ProductInfo"; 
+import BrandInfo from "./BrandInfo"; 
+import PaymentInfo from "./PaymentInfo";
 
 export default function () {
-  
- 
   return (
-    <div className="flex flex-col items-center justify-start w-full h-full"> 
+    <div className="flex flex-col items-center justify-start w-full h-full overflow-y-auto"> 
       <CardInfo />
       <OrderInfo/>
-      <div className="flex flex-col w-full mt-2 md:flex-row">
+      <div className="flex flex-col w-full mt-4 md:flex-row">
         <ProductInfo />
+        <BrandInfo />
       </div>
+      <div className="flex flex-col w-full mt-4 md:flex-row">
+        <PaymentInfo />
+        </div>
     </div>
   );
 }
