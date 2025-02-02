59b23271695d63ae4dededcc12a29f18ed04d4c8
Jay200123 <kenken.ayonon@gmail.com>
2025-02-02 17:50:00 +0800

    Add OrderStatus component to Dashboard layout

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,21 +1,23 @@
 import CardInfo from "./CardInfo";
 import OrderInfo from "./OrderInfo";
-import ProductInfo from "./ProductInfo"; 
-import BrandInfo from "./BrandInfo"; 
+import ProductInfo from "./ProductInfo";
+import BrandInfo from "./BrandInfo";
 import PaymentInfo from "./PaymentInfo";
+import OrderStatus from "./OrderStatus";
 
 export default function () {
   return (
-    <div className="flex flex-col items-center justify-start w-full h-full overflow-y-auto"> 
+    <div className="flex flex-col items-center justify-start w-full h-full overflow-y-auto">
       <CardInfo />
-      <OrderInfo/>
+      <OrderInfo />
       <div className="flex flex-col w-full mt-4 md:flex-row">
         <ProductInfo />
         <BrandInfo />
       </div>
       <div className="flex flex-col w-full mt-4 md:flex-row">
         <PaymentInfo />
-        </div>
+        <OrderStatus />
+      </div>
     </div>
   );
 }
