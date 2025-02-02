e29f72fe0c4aa65614d5769aaa35a1f3de837ee1
Jay200123 <kenken.ayonon@gmail.com>
2025-02-02 20:08:12 +0800

    Add validation schema for editing user profile using Yup

---
+++ src/validations/editProfileValidation.jsx
@@ -0,0 +1,11 @@
+import * as yup from "yup";
+
+const editProfileValidationSchema = yup.object({
+  fullname: yup.string().required("Fullname is required"),
+  contact_number: yup.string().required("Contact number is required"),
+  address: yup.string().required("Address is required"),
+  city: yup.string().required("City is required"),
+  email: yup.string().email("Invalid email").required("Email is required"),
+});
+
+export default editProfileValidationSchema;
