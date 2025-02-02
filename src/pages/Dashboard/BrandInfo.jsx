1743b88660561a7a24142ac9fcf0e082297301d1
Jay200123 <kenken.ayonon@gmail.com>
2025-02-02 14:26:48 +0800

    Add BrandInfo component to display product distribution by brand

---
+++ src/pages/Dashboard/BrandInfo.jsx
@@ -0,0 +1,72 @@
+import { useGetProductsQuery } from "../../state/api/reducer";
+import { Pie } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { FadeLoader } from "react-spinners";
+
+export default function () {
+  ChartJS.register(ArcElement, Tooltip, Legend);
+
+  const { data, isLoading } = useGetProductsQuery();
+  const products = data?.details || [];
+
+  const productBrands = products?.reduce((acc, product) => {
+    const brand = product?.brand?.brand_name;
+    acc[brand] = acc[brand] ? acc[brand] + 1 : 1;
+    return acc;
+  }, {});
+
+  const generateRandomColors = (colors) => {
+    return Array.from(
+      { length: colors },
+      () =>
+        `hsl(${Math.floor(Math.random() * 360)}, ${50 + Math.random() * 30}%, ${
+          40 + Math.random() * 20
+        }%)`
+    );
+  };
+
+  const chartData = {
+    labels: Object.keys(productBrands),
+    datasets: [
+      {
+        label: "Products for each brand",
+        data: Object.values(productBrands),
+        backgroundColor: generateRandomColors(
+          Object.keys(productBrands).length
+        ),
+      },
+    ],
+  };
+
+  const pieOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      tooltip: {
+        callbacks: {
+          label: function (tooltipItem) {
+            const brand = tooltipItem.label;
+            const count = tooltipItem.raw;
+            return `${brand}: ${count} products`;
+          },
+        },
+      },
+    },
+  };
+
+  return (
+    <div className="w-1/2 m-4 flex items-center justify-center max-h-[250px] md:h-[350px]">
+      {isLoading ? (
+        <div className="flex items-center justify-center">
+          <FadeLoader color="#808080" loading={true} height={15} width={5} />
+        </div>
+      ) : (
+        <>
+          <Pie data={chartData} options={pieOptions} />
+        </>
+      )}
+    </div>
+  );
+}
