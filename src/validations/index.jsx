c80140d4a36bcb7598ca64af2d686e5eda7accec
Jay200123 <kenken.ayonon@gmail.com>
2025-02-02 20:13:18 +0800

    Add validation schemas for brand and product management

--- src/validations/index.jsx
+++ src/validations/index.jsx
@@ -1,5 +1,9 @@
 import SignInValidationSchema from "./signInValidation";
 import SignUpValidationSchema from "./signUpValidation";
+import createBrandValidationSchema from "./createBrandValidation";
+import editBrandValidationSchema from "./editBrandValidation";  
+import createProductValidationSchema from "./createProductValidation";  
+import editProductValidationSchema from "./editProductValidation";  
 
 export {
     SignInValidationSchema,
