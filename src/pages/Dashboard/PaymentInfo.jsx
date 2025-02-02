943fd140780eab0b4a8f950f4ffd8fa0ac6120b8
Jay200123 <kenken.ayonon@gmail.com>
2025-02-02 17:50:21 +0800

    Add PaymentInfo component to visualize orders by payment method with a pie chart

--- src/pages/Dashboard/PaymentInfo.jsx
+++ src/pages/Dashboard/PaymentInfo.jsx
@@ -1,6 +1,11 @@
 import { useGetOrdersQuery } from "../../state/api/reducer";
+import generateRandomColors from "../../utils/randomColors";
+import { FadeLoader } from "react-spinners";
+import { Pie } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 
 export default function () {
+  ChartJS.register(ArcElement, Tooltip, Legend);
   const { data, isLoading } = useGetOrdersQuery();
   const orders = data?.details || [];
 
@@ -9,5 +14,48 @@ export default function () {
     return acc;
   }, {});
 
-  return <>Setting up payment dashboard!</>;
+  const chartData = {
+    labels: Object.keys(orderPayments),
+    datasets: [
+      {
+        label: "Orders by payment method",
+        data: Object.values(orderPayments),
+        backgroundColor: generateRandomColors(
+          Object.keys(orderPayments).length
+        ),
+      },
+    ],
+  };
+
+  const pieOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      tooltip: {
+        callbacks: {
+          label: function (tooltipItem) {
+            const brand = tooltipItem.label;
+            const count = tooltipItem.raw;
+            return `${brand}: ${count} products`;
+          },
+        },
+      },
+    },
+  };
+
+  return (
+    <div className="w-1/2 m-4 flex items-center justify-center max-h-[250px] md:h-[350px]">
+      {isLoading ? (
+        <div className="flex items-center justify-center">
+          <FadeLoader color="#808080" loading={true} height={15} width={5} />
+        </div>
+      ) : (
+        <>
+          <Pie data={chartData} options={pieOptions} />
+        </>
+      )}
+    </div>
+  );
 }
