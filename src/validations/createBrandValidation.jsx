d10dcecdc6733b681d893e96a03b97810ce11957
Jay200123 <kenken.ayonon@gmail.com>
2025-02-02 20:13:03 +0800

    Rename createBrandValidation to createBrandValidationSchema for consistency

--- src/validations/createBrandValidation.jsx
+++ src/validations/createBrandValidation.jsx
@@ -1,7 +1,7 @@
 import * as yup from "yup";
 
-const createBrandValidation = yup.object({
+const createBrandValidationSchema = yup.object({
     brand_name: yup.string().required("Brand name is required"),
 });
 
-export default createBrandValidation;   
+export default createBrandValidationSchema;   
