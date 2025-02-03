28db2339cfcaaf196dbc5d5d5bb112c172f5ad65
Jay200123 <kenken.ayonon@gmail.com>
2025-02-03 10:18:15 +0800

    Add PATH constant to exports in constants index

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,9 +1,11 @@
 import TAGS from "./tags";
 import API from "./api";
 import RESOURCE from "./resource";
+import PATH from "./path";  
 
 export {
   TAGS,
   API,
-  RESOURCE
+  RESOURCE,
+  PATH,
 };
