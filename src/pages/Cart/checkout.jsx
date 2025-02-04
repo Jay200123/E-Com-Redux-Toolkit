5e87e2735045b6744c98b3f9465922ff0e0857e8
Jay200123 <kenken.ayonon@gmail.com>
2025-02-04 17:56:27 +0800

    Redirect to user orders page after successful checkout

--- src/pages/Cart/checkout.jsx
+++ src/pages/Cart/checkout.jsx
@@ -45,7 +45,7 @@ export default function () {
 
       if (res?.data?.success === true) {
         toast.success(res.data.message);
-        navigate("/");
+        navigate("/user/orders"); 
       } else if (res?.error?.data?.success === false) {
         toast.error(`${res?.error?.data?.error.message}`);
       }
