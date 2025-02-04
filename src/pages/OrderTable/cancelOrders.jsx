ca2043e64396d14d2a17b8bad95db6493892121d
Jay200123 <kenken.ayonon@gmail.com>
2025-02-04 18:08:17 +0800

    Add approveCancelOrder mutation and update OrderTable to handle approved cancellations

--- src/pages/OrderTable/cancelOrders.jsx
+++ src/pages/OrderTable/cancelOrders.jsx
@@ -1,229 +1,158 @@
 import {
-    useGetOrdersQuery,
-    useDeleteOrderMutation,
-    usePackedOrderMutation,
-    useShippedOrderMutation,
-    useDeliverOrderMutation,
-  } from "../../state/api/reducer";
-  import DataTable from "react-data-table-component";
-  import { FadeLoader } from "react-spinners";
-  import { tableCustomStyles } from "../../utils/tableCustomStyle";
-  import { useNavigate } from "react-router-dom";
-  import { FaBox, FaShip, FaTruckMoving, FaTrash, FaEye } from "react-icons/fa";
-  import { useState } from "react";
-  import { toast } from "react-toastify";
-  import { useEffect, useRef } from "react";
-  
-  export default function () {
-    const navigate = useNavigate();
-    const isFocused = useRef(true);
-  
-    const { data, isLoading, refetch } = useGetOrdersQuery();
-    const orders = data?.details;
-  
-    const processedOrders = orders?.filter((o) => o?.isCancelled === true || o?.isCancelApproved === true); 
-  
-    useEffect(() => {
-      const handleFocus = () => {
-        isFocused.current = true;
-        refetch();
-      };
-  
-      window.addEventListener("focus", handleFocus);
-  
-      return () => {
-        window.removeEventListener("focus", handleFocus);
-      };
-    }, [refetch]);
-  
-    const [packedOrder] = usePackedOrderMutation();
-    const [shippedOrder] = useShippedOrderMutation();
-    const [deliverOrder] = useDeliverOrderMutation();
-    const [deleteOrder] = useDeleteOrderMutation();
-  
-    const handlePackedOrder = async (id) => {
-      if (window.confirm("Order packed?")) {
-        await packedOrder(id);
-        toast.success("Order packed successfully");
-      }
+  useGetOrdersQuery,
+  useApprovedCancelOrderMutation,
+} from "../../state/api/reducer";
+import DataTable from "react-data-table-component";
+import { FadeLoader } from "react-spinners";
+import { tableCustomStyles } from "../../utils/tableCustomStyle";
+import { useNavigate } from "react-router-dom";
+import { FaCheck } from "react-icons/fa";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { useEffect, useRef } from "react";
+
+export default function () {
+  const navigate = useNavigate();
+  const isFocused = useRef(true);
+
+  const { data, isLoading, refetch } = useGetOrdersQuery();
+  const orders = data?.details;
+  const [approvedCancelOrder] = useApprovedCancelOrderMutation();
+
+  const processedOrders = orders?.filter(
+    (o) => o?.isCancelled === true && o?.isCancelApproved === true
+  );
+
+  useEffect(() => {
+    const handleFocus = () => {
+      isFocused.current = true;
+      refetch();
     };
-  
-    const handleShippedOrder = async (id) => {
-      if (window.confirm("Order Shipped?")) {
-        await shippedOrder(id);
-        refetch();
-        toast.success("Order shipped successfully");
-      }
+
+    window.addEventListener("focus", handleFocus);
+
+    return () => {
+      window.removeEventListener("focus", handleFocus);
     };
-  
-    const handleDeliverOrder = async (id) => {
-      if (window.confirm("Order Shipped?")) {
-        await deliverOrder(id);
-        refetch();
-        toast.success("Order delivered successfully");
-      }
-    };
-  
-    const handleDelete = async (id) => {
-      if (window.confirm("Delete Order Record?")) {
-        await deleteOrder(id);
-        refetch();
-        toast.success("Order deleted successfully");
-      }
-    };
-  
-    const [order, setOrder] = useState("");
-  
-    const filteredOrders = processedOrders?.filter((o) =>
-      o?.orderNumber?.toLowerCase()?.includes(order?.toLowerCase())
-    );
-  
-    const columns = [
-      {
-        name: "OR Number",
-        selector: (row) => row?.orderNumber,
-        sortable: true,
+  }, [refetch]);
+
+  const handleApprove = async (id) => {
+    if (window.confirm("Approve Cancel Request?")) {
+      await approvedCancelOrder(id);
+      refetch();
+      toast.success("Cancel request approved successfully");
+    }
+  };
+
+  const [order, setOrder] = useState("");
+
+  const filteredOrders = processedOrders?.filter((o) =>
+    o?.orderNumber?.toLowerCase()?.includes(order?.toLowerCase())
+  );
+
+  const columns = [
+    {
+      name: "OR Number",
+      selector: (row) => row?.orderNumber,
+      sortable: true,
+    },
+    {
+      name: "User",
+      selector: (row) => row?.user?.fullname,
+      sortable: true,
+    },
+    {
+      name: "Ordered Items",
+      cell: (row) => {
+        return (
+          <ul className="space-y-1 text-sm list-disc list-inside">
+            {row?.products?.map((p, index) => (
+              <li className="truncate" key={index}>
+                {p?.product?.product_name}{" "}
+                <span className="font-medium">x {p?.quantity}</span>
+              </li>
+            ))}
+          </ul>
+        );
       },
-      {
-        name: "User",
-        selector: (row) => row?.user?.fullname,
-        sortable: true,
-      },
-      {
-        name: "Ordered Items",
-        cell: (row) => {
-          return (
-            <ul className="space-y-1 text-sm list-disc list-inside">
-              {row?.products?.map((p, index) => (
-                <li className="truncate" key={index}>
-                  {p?.product?.product_name}{" "}
-                  <span className="font-medium">x {p?.quantity}</span>
-                </li>
-              ))}
-            </ul>
-          );
-        },
-      },
-      {
-        name: "Status",
-        selector: (row) => row?.status,
-        sortable: true,
-      },
-      {
-        name: "Payment",
-        selector: (row) => row?.payment,
-        sortable: true,
-      },
-      {
-        name: "Date Placed",
-        selector: (row) =>
-          new Date(row?.date_placed.toLocaleString()).toISOString().split("T")[0],
-        sortable: true,
-      },
-      {
-        name: "Date delivered",
-        selector: (row) =>
-          row?.date_delivered
-            ? new Date(row?.date_delivered.toLocaleString())
-                .toISOString()
-                .split("T")[0]
-            : "Not yet delivered",
-        sortable: true,
-      },
-      {
-        name: "Actions",
-        cell: (row) => (
-          <div className="flex items-center text-center">
-            {row?.status === "Packed" ||
-            row?.status === "Shipped" ||
-            row?.status === "Delivered" ? (
-              <FaBox
-                onClick={() => toast.error("Order already packed")}
-                title="Packed Order"
-                className="mr-2 text-xl text-gray-500"
-              />
-            ) : (
-              <FaBox
-                onClick={() => handlePackedOrder(row?._id)}
-                title="Packed Order"
-                className="mr-2 text-xl text-yellow-500"
-              />
-            )}
-  
-            {row?.status === "Shipped" || row?.status === "Delivered" ? (
-              <FaShip
-                onClick={() => toast.error(" Order already shipped")}
-                title="Shipped Order"
-                className="mr-2 text-xl text-gray-500"
-              />
-            ) : (
-              <FaShip
-                onClick={() => handleShippedOrder(row?._id)}
-                title="Shipped Order"
-                className="mr-2 text-xl text-blue-500"
-              />
-            )}
-  
-            {row?.status === "Delivered" ? (
-              <FaTruckMoving
-                onClick={() => toast.error("Order already delivered")}
-                title="Deliver Order"
-                className="mr-2 text-xl text-gray-500"
-              />
-            ) : (
-              <FaTruckMoving
-                onClick={() => handleDeliverOrder(row?._id)}
-                title="Deliver Order"
-                className="mr-2 text-xl text-green-500"
-              />
-            )}
-  
-            <FaEye
-              onClick={() => navigate(`/order/${row?._id}`)}
-              title="View Order"
-              className="mr-1 text-xl text-gray-500"
+    },
+    {
+      name: "Status",
+      selector: (row) => row?.status,
+      sortable: true,
+    },
+    {
+      name: "Payment",
+      selector: (row) => row?.payment,
+      sortable: true,
+    },
+    {
+      name: "Date Placed",
+      selector: (row) =>
+        new Date(row?.date_placed.toLocaleString()).toISOString().split("T")[0],
+      sortable: true,
+    },
+    {
+      name: "Date delivered",
+      selector: (row) =>
+        row?.date_delivered
+          ? new Date(row?.date_delivered.toLocaleString())
+              .toISOString()
+              .split("T")[0]
+          : "Not yet delivered",
+      sortable: true,
+    },
+    {
+      name: "Actions",
+      cell: (row) => (
+        <div className="flex items-center text-center">
+          {row?.isCancelApproved ? (
+            <FaCheck
+              onClick={() => toast.error(" Cancel request already approved")}
+              title="Approved Cancel Request"
+              className="mr-1 text-xl text-grey-50"
             />
-  
-            <FaTrash
-              onClick={() => handleDelete(row?._id)}
-              title="Delete Order"
-              className="mr-1 text-xl text-red-500"
+          ) : (
+            <FaCheck
+              onClick={() => handleApprove(row?._id)}
+              title="Approved Cancel Request"
+              className="mr-1 text-xl text-green-500"
+            />
+          )}
+        </div>
+      ),
+    },
+  ];
+
+  return (
+    <div className="w-full h-full overflow-x-auto">
+      {isLoading ? (
+        <div className="flex items-center justify-center">
+          <FadeLoader color="#808080" loading={true} height={15} width={5} />
+        </div>
+      ) : (
+        <div className="max-w-3xl m-4 rounded-md shadow-md md:max-w-5xl">
+          <div className="w-full m-2">
+            <input
+              type="text"
+              placeholder="Enter OR Number"
+              onChange={(e) => setOrder(e.target.value)}
+              className="p-2 text-sm border border-black rounded-md"
             />
           </div>
-        ),
-      },
-    ];
-  
-    return (
-      <div className="w-full h-full overflow-x-auto">
-        {isLoading ? (
-          <div className="flex items-center justify-center">
-            <FadeLoader color="#808080" loading={true} height={15} width={5} />
-            </div>
-        ) : (
-          <div className="max-w-3xl m-4 rounded-md shadow-md md:max-w-5xl">
-            <div className="w-full m-2">
-              <input
-                type="text"
-                placeholder="Enter OR Number"
-                onChange={(e) => setOrder(e.target.value)}
-                className="p-2 text-sm border border-black rounded-md"
-              />
-            </div>
-            <DataTable
-              title="Cancelled Orders"
-              columns={columns}
-              data={filteredOrders || []}
-              pagination
-              highlightOnHover
-              pointerOnHover
-              paginationPerPage={10}
-              paginationRowsPerPageOptions={[10, 20, 30]}
-              customStyles={tableCustomStyles}
-            />
-          </div>
-        )}
-      </div>
-    );
-  }
-  
+          <DataTable
+            title="Cancelled Orders"
+            columns={columns}
+            data={filteredOrders || []}
+            pagination
+            highlightOnHover
+            pointerOnHover
+            paginationPerPage={10}
+            paginationRowsPerPageOptions={[10, 20, 30]}
+            customStyles={tableCustomStyles}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
