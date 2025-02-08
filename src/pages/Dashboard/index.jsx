eefe35a5f1dc779f84de5f794a7ad5224132f08b
Jay200123 <kenken.ayonon@gmail.com>
2025-02-08 20:48:05 +0800

    ``` Add framer-motion animations to Footer component ```

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -4,10 +4,17 @@ import ProductInfo from "./ProductInfo";
 import BrandInfo from "./BrandInfo";
 import PaymentInfo from "./PaymentInfo";
 import OrderStatus from "./OrderStatus";
+import { motion } from "framer-motion";
 
 export default function () {
   return (
-    <div className="flex flex-col items-center justify-start w-full overflow-y-auto">
+    <motion.div
+      initial={{ y: 100, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.5, ease: "easeInOut" }}
+      viewport={{ visible: 1, once: false }}
+      className="flex flex-col items-center justify-start w-full overflow-y-auto"
+    >
       <CardInfo />
       <OrderInfo />
       <div className="flex flex-col w-full mt-4 md:flex-row">
@@ -18,6 +25,6 @@ export default function () {
         <PaymentInfo />
         <OrderStatus />
       </div>
-    </div>
+    </motion.div>
   );
 }
