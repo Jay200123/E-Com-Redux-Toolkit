6c84b81722397bab5e542f3626cb3a6ad562c519
Jay200123 <kenken.ayonon@gmail.com>
2025-02-08 20:30:49 +0800

    Add framer-motion animations to Navbar and Footer in MainLayout

--- src/layout/MainLayout/index.jsx
+++ src/layout/MainLayout/index.jsx
@@ -1,14 +1,30 @@
 import { Outlet } from "react-router-dom";
 import { Navbar, Footer } from "../../components";
+import { motion } from "framer-motion";
 
 export default function () {
   return (
     <main className="flex flex-col justify-between min-h-screen transition-all duration-500 min-w-screen">
-      <Navbar />
+      <motion.div
+        initial={{ y: -100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1 }}
+        viewport={{ visible: 1, once: false }}
+      >
+        <Navbar />
+      </motion.div>
+
       <div>
         <Outlet />
       </div>
-      <Footer />
+      <motion.div
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1 }}
+        viewport={{ visible: 1, once: false }}
+      >
+        <Footer />
+      </motion.div>
     </main>
   );
 }
