9f03d307d8ad2fec447a7bad1c9691edc0a6e53d
Jay200123 <kenken.ayonon@gmail.com>
2025-02-08 20:27:35 +0800

    Update viewport settings for Home page animation

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -95,7 +95,7 @@ export default function () {
         initial={{ x: -100, opacity: 0 }}
         animate={{ x: 0, opacity: 1 }}
         transition={{ duration: 0.8, ease: "easeOut" }}
-        viewport={{ amount: 0.5 }}
+        viewport={{ amount: 0.5, once: false }}
       >
         <Carousel />
       </motion.div>
