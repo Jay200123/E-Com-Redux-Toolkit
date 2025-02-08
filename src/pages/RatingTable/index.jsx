3aac0ee1153637b6c2ded80f1fcab93d878422ff
Jay200123 <kenken.ayonon@gmail.com>
2025-02-08 20:48:22 +0800

    Add framer-motion animations to RatingTable component

--- src/pages/RatingTable/index.jsx
+++ src/pages/RatingTable/index.jsx
@@ -9,6 +9,7 @@ import { tableCustomStyles } from "../../utils/tableCustomStyle";
 import { useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import { motion } from "framer-motion";
 
 export default function () {
   const navigate = useNavigate();
@@ -115,9 +116,10 @@ export default function () {
       name: "Actions",
       cell: (row) => (
         <div className="flex items-center">
-          <FaEye 
-          onClick={() => navigate(`/admin/rating/${row?._id}`)} 
-          className="mr-1 text-lg text-green-500 cursor-pointer" />
+          <FaEye
+            onClick={() => navigate(`/admin/rating/${row?._id}`)}
+            className="mr-1 text-lg text-green-500 cursor-pointer"
+          />
           <FaTrash
             onClick={() => handleDelete(row?._id)}
             className="mr-1 text-lg text-red-500 cursor-pointer"
@@ -128,7 +130,13 @@ export default function () {
   ];
 
   return (
-    <div className="w-full overflow-x-auto">
+    <motion.div
+      initial={{ y: 100, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.5, ease: "easeInOut" }}
+      viewport={{ visible: 0.8, once: false }}
+      className="w-full overflow-x-auto"
+    >
       {isLoading ? (
         <div className="flex items-center justify-center">
           <FadeLoader color="#808080" loading={true} height={15} width={5} />
@@ -156,6 +164,6 @@ export default function () {
           />
         </div>
       )}
-    </div>
+    </motion.div>
   );
 }
