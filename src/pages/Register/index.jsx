5c33254480b1bc0ef9eef68598414aea5cd443ce
Jay200123 <kenken.ayonon@gmail.com>
2025-02-08 20:00:55 +0800

    Add framer-motion animations to the registration form

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import ImageOne from "../../assets/register.jpg";
 import { SignUpValidationSchema } from "../../validations";
 import { useState } from "react";
+import { motion } from "framer-motion"; 
 
 export default function () {
   const navigate = useNavigate();
@@ -60,153 +61,175 @@ export default function () {
   };
 
   return (
-    <form
-      className="flex relative justify-center transition-all duration-700 w-full h-[36rem] md:h-[45rem] p-3 bg-white md:p-1 mt-2"
-      onSubmit={formik.handleSubmit}
+    <motion.div
+      initial={{ y: 100, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      exit={{ y: 100, opacity: 0 }}
+      transition={{ duration: 0.5, ease: "easeInOut" }}
+      viewport={{ visible: 0.5 }}
     >
-      <i
-        onClick={back}
-        className="absolute top-0 left-0 m-2 text-2xl cursor-pointer fa fa-arrow-left"
-      ></i>
-      <div className="hidden md:w-1/2 md:block">
-        <img
-          className="object-cover w-full h-full rounded-sm"
-          src={ImageOne}
-          alt="Image"
-        />
-      </div>
-      <div className="flex flex-col justify-center w-full p-4 overflow-y-auto rounded-md md:border md:border-gray-500 md:h-full md:w-1/2">
-        <h3 className="text-lg md:text-3xl">Sign Up</h3>
-        <p className="mb-4 text-xs md:text-[1rem]">
-          Already have an account?
-          <span
-            onClick={signIn}
-            className="ml-1 font-bold underline cursor-pointer"
+      <form
+        className="flex relative justify-center transition-all duration-700 w-full h-[36rem] md:h-[45rem] p-3 bg-white md:p-1 mt-2"
+        onSubmit={formik.handleSubmit}
+      >
+        <i
+          onClick={back}
+          className="absolute top-0 left-0 m-2 text-2xl cursor-pointer fa fa-arrow-left"
+        ></i>
+        <motion.div 
+        initial={{ x: -100, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ duration: 0.8, ease: "easeOut" }}
+        viewport={{ amount: 0.5, once: false }}
+        className="hidden md:w-1/2 md:block">
+          <img
+            className="object-cover w-full h-full rounded-sm"
+            src={ImageOne}
+            alt="Image"
+          />
+        </motion.div>
+        <motion.div 
+        initial={{ x: 100, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ duration: 0.8, ease: "easeOut" }}
+        viewport={{ amount: 0.5, once: false }}
+        className="flex flex-col justify-center w-full p-4 overflow-y-auto rounded-md md:border md:border-gray-500 md:h-full md:w-1/2">
+          <h3 className="text-lg md:text-3xl">Sign Up</h3>
+          <p className="mb-4 text-xs md:text-[1rem]">
+            Already have an account?
+            <span
+              onClick={signIn}
+              className="ml-1 font-bold underline cursor-pointer"
+            >
+              Sign In
+            </span>
+          </p>
+          <div className="flex flex-col mt-2">
+            <label className="text-sm font-medium md:text-lg">Fullname</label>
+            <input
+              name="fullname"
+              id="fullname"
+              className="p-2 text-sm border border-black rounded-md"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              value={formik.values.fullname}
+              type="text"
+            />
+            {formik.touched.fullname && formik.errors.fullname && (
+              <p className="mt-1 text-sm text-red-500">
+                {formik.errors.fullname}
+              </p>
+            )}
+          </div>
+          <div className="flex flex-col mt-2">
+            <label className="text-sm font-medium md:text-lg">
+              Contact Number
+            </label>
+            <input
+              name="contact_number"
+              id="contact_number"
+              className="p-2 text-sm border border-black rounded-md"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              value={formik.values.contact_number}
+              type="text"
+            />
+            {formik.touched.contact_number && formik.errors.contact_number && (
+              <p className="mt-1 text-sm text-red-500">
+                {formik.errors.contact_number}
+              </p>
+            )}
+          </div>
+          <div className="flex flex-col mt-2">
+            <label className="text-sm font-medium md:text-lg">Address</label>
+            <input
+              name="address"
+              id="address"
+              className="p-2 text-sm border border-black rounded-md"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              value={formik.values.address}
+              type="text"
+            />
+            {formik.touched.address && formik.errors.address && (
+              <p className="mt-1 text-sm text-red-500">
+                {formik.errors.address}
+              </p>
+            )}
+          </div>
+          <div className="flex flex-col mt-2">
+            <label className="text-sm font-medium md:text-lg">City</label>
+            <input
+              name="city"
+              id="city"
+              className="p-2 text-sm border border-black rounded-md"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              value={formik.values.city}
+              type="text"
+            />
+            {formik.touched.city && formik.errors.city && (
+              <p className="mt-1 text-sm text-red-500">{formik.errors.city}</p>
+            )}
+          </div>
+          <div className="flex flex-col mt-2">
+            <label className="text-sm font-medium md:text-lg">Email</label>
+            <input
+              name="email"
+              id="email"
+              className="p-2 text-sm border border-black rounded-md"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              value={formik.values.email}
+              type="text"
+            />
+            {formik.touched.email && formik.errors.email && (
+              <p className="mt-1 text-sm text-red-500">{formik.errors.email}</p>
+            )}
+          </div>
+
+          <div className="flex flex-col mt-2">
+            <label className="text-sm font-medium md:text-lg">Password</label>
+            <input
+              name="password"
+              id="password"
+              className="p-2 text-sm border border-black rounded-md"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              value={formik.values.password}
+              type={isShow ? "text" : "password"}
+            />
+            {formik.touched.password && formik.errors.password && (
+              <p className="mt-1 text-sm text-red-500">
+                {formik.errors.password}
+              </p>
+            )}
+          </div>
+
+          <div className="flex flex-col mt-2">
+            <input
+              type="file"
+              name="image"
+              multiple
+              className="text-xs md:text-sm"
+              onChange={(e) => {
+                const files = Array.from(e.currentTarget.files || []);
+                formik.setFieldValue("image", files);
+              }}
+            />
+          </div>
+
+          <button
+            disabled={!formik.isValid || formik.isSubmitting}
+            className={`text-[1rem] p-2 bg-black  transition-all duration-500 hover:opacity-75 rounded-md text-white mt-4 ${
+              !formik?.isValid && "cursor-not-allowed opacity-50"
+            }`}
+            type="submit"
           >
-            Sign In
-          </span>
-        </p>
-        <div className="flex flex-col mt-2">
-          <label className="text-sm font-medium md:text-lg">Fullname</label>
-          <input
-            name="fullname"
-            id="fullname"
-            className="p-2 text-sm border border-black rounded-md"
-            onBlur={formik.handleBlur}
-            onChange={formik.handleChange}
-            value={formik.values.fullname}
-            type="text"
-          />
-          {formik.touched.fullname && formik.errors.fullname && (
-            <p className="mt-1 text-sm text-red-500">
-              {formik.errors.fullname}
-            </p>
-          )}
-        </div>
-        <div className="flex flex-col mt-2">
-          <label className="text-sm font-medium md:text-lg">
-            Contact Number
-          </label>
-          <input
-            name="contact_number"
-            id="contact_number"
-            className="p-2 text-sm border border-black rounded-md"
-            onBlur={formik.handleBlur}
-            onChange={formik.handleChange}
-            value={formik.values.contact_number}
-            type="text"
-          />
-          {formik.touched.contact_number && formik.errors.contact_number && (
-            <p className="mt-1 text-sm text-red-500">
-              {formik.errors.contact_number}
-            </p>
-          )}
-        </div>
-        <div className="flex flex-col mt-2">
-          <label className="text-sm font-medium md:text-lg">Address</label>
-          <input
-            name="address"
-            id="address"
-            className="p-2 text-sm border border-black rounded-md"
-            onBlur={formik.handleBlur}
-            onChange={formik.handleChange}
-            value={formik.values.address}
-            type="text"
-          />
-          {formik.touched.address && formik.errors.address && (
-            <p className="mt-1 text-sm text-red-500">{formik.errors.address}</p>
-          )}
-        </div>
-        <div className="flex flex-col mt-2">
-          <label className="text-sm font-medium md:text-lg">City</label>
-          <input
-            name="city"
-            id="city"
-            className="p-2 text-sm border border-black rounded-md"
-            onBlur={formik.handleBlur}
-            onChange={formik.handleChange}
-            value={formik.values.city}
-            type="text"
-          />
-          {formik.touched.city && formik.errors.city && (
-            <p className="mt-1 text-sm text-red-500">{formik.errors.city}</p>
-          )}
-        </div>
-        <div className="flex flex-col mt-2">
-          <label className="text-sm font-medium md:text-lg">Email</label>
-          <input
-            name="email"
-            id="email"
-            className="p-2 text-sm border border-black rounded-md"
-            onBlur={formik.handleBlur}
-            onChange={formik.handleChange}
-            value={formik.values.email}
-            type="text"
-          />
-          {formik.touched.email && formik.errors.email && (
-            <p className="mt-1 text-sm text-red-500">{formik.errors.email}</p>
-          )}
-        </div>
-
-        <div className="flex flex-col mt-2">
-          <label className="text-sm font-medium md:text-lg">Password</label>
-          <input
-            name="password"
-            id="password"
-            className="p-2 text-sm border border-black rounded-md"
-            onBlur={formik.handleBlur}
-            onChange={formik.handleChange}
-            value={formik.values.password}
-            type={isShow ? "text" : "password"}
-          />
-          {formik.touched.password && formik.errors.password && (
-            <p className="mt-1 text-sm text-red-500">
-              {formik.errors.password}
-            </p>
-          )}
-        </div>
-
-        <div className="flex flex-col mt-2">
-          <input
-            type="file"
-            name="image"
-            multiple
-            className="text-xs md:text-sm"
-            onChange={(e) => {
-              const files = Array.from(e.currentTarget.files || []);
-              formik.setFieldValue("image", files);
-            }}
-          />
-        </div>
-
-        <button
-          disabled={!formik.isValid || formik.isSubmitting}
-          className={`text-[1rem] p-2 bg-black  transition-all duration-500 hover:opacity-75 rounded-md text-white mt-4 ${!formik?.isValid && "cursor-not-allowed opacity-50"}`}
-          type="submit"
-        >
-          Sign Up
-        </button>
-      </div>
-    </form>
+            Sign Up
+          </button>
+        </motion.div>
+      </form>
+    </motion.div>
   );
 }
