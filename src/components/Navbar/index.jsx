a3475103dd6786c7f93f990119edeebae1df6139
Jay200123 <kenken.ayonon@gmail.com>
2025-02-13 21:28:58 +0800

    Add framer-motion animations to Navbar component for enhanced user interaction

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 import { useLogoutMutation } from "../../state/api/reducer";
+import { motion } from "framer-motion";
 
 export default function () {
   const navigate = useNavigate();
@@ -45,49 +46,90 @@ export default function () {
 
   return (
     <nav className="flex items-center justify-between w-full h-16 overflow-hidden shadow-md">
-      <h3
+      <motion.h3
+        whileHover={{
+          scale: 0.9,
+          transition: { duration: 0.8 },
+        }}
+        whileTap={{ scale: 0.9 }}
         onClick={() => navigate("/")}
         className="text-sm font-medium truncate cursor-pointer md:text-2xl md:font-bold"
       >
         <i className="mr-1 fa-solid fa-screwdriver-wrench"></i>Tech Fix
-      </h3>
+      </motion.h3>
       <div className="hidden md:block">
         <ul className="flex flex-row items-center overflow-x-auto">
-          <li
+          <motion.li
+            whileHover={{
+              scale: 0.9,
+              transition: { duration: 0.8 },
+            }}
+            whileTap={{ scale: 0.9 }}
             onClick={handleShop}
             className="mr-1 text-xs font-medium cursor-pointer md:font-bold md:mr-2 md:text-sm"
           >
             <i className="mr-[2px] fa-solid fa-bag-shopping"></i> Shop
-          </li>
+          </motion.li>
           {auth?.isAuthenticated ? (
             <>
-              <li
+              <motion.li
+                whileHover={{
+                  scale: 0.9,
+                  transition: { duration: 0.8 },
+                }}
+                whileTap={{ scale: 0.9 }}
                 onClick={handleOrders}
                 className="block mr-1 text-xs font-medium md:hidden md:font-bold md:mr-2 md:text-sm"
               >
                 Orders
-              </li>
-              <li className="block mr-1 text-xs font-medium md:hidden md:font-bold md:mr-2 md:text-sm">
+              </motion.li>
+              <motion.li
+                whileHover={{
+                  scale: 0.9,
+                  transition: { duration: 0.8 },
+                }}
+                whileTap={{ scale: 0.9 }}
+                className="block mr-1 text-xs font-medium md:hidden md:font-bold md:mr-2 md:text-sm"
+              >
                 Reviews
-              </li>
+              </motion.li>
             </>
           ) : (
             <></>
           )}
 
-          <li className="mr-1 text-xs font-medium md:font-bold md:mr-2 md:text-sm">
+          <motion.li
+            whileHover={{
+              scale: 0.9,
+              transition: { duration: 0.8 },
+            }}
+            whileTap={{ scale: 0.9 }}
+            className="mr-1 text-xs font-medium md:font-bold md:mr-2 md:text-sm"
+          >
             <i className="mr-1 fa-solid fa-circle-info"></i> About
-          </li>
-          <li className="mr-1 text-xs font-medium truncate md:font-bold md:mr-2 md:text-sm">
+          </motion.li>
+          <motion.li
+            whileHover={{
+              scale: 0.9,
+              transition: { duration: 0.8 },
+            }}
+            whileTap={{ scale: 0.9 }}
+            className="mr-1 text-xs font-medium truncate md:font-bold md:mr-2 md:text-sm"
+          >
             <i className="mr-1 fa-solid fa-phone"></i>Contact Us
-          </li>
+          </motion.li>
         </ul>
       </div>
       <div className="hidden md:block">
         <ul className="flex flex-row items-center">
           {auth?.isAuthenticated ? (
             <>
-              <li
+              <motion.li
+                whileHover={{
+                  scale: 1.2,
+                  transition: { duration: 0.8 },
+                }}
+                whileTap={{ scale: 1.2 }}
                 onClick={myCart}
                 className="relative p-2 mr-4 text-lg font-medium cursor-pointer md:font-bold"
               >
@@ -95,8 +137,13 @@ export default function () {
                   {cart?.item?.length || 0}
                 </span>
                 <i className="fa-solid fa-cart-shopping"></i>
-              </li>
-              <li
+              </motion.li>
+              <motion.li
+                whileHover={{
+                  scale: 0.9,
+                  transition: { duration: 0.8 },
+                }}
+                whileTap={{ scale: 0.9 }}
                 onClick={() => navigate("/profile")}
                 className="space-x-1 text-xs font-medium cursor-pointer md:font-bold md:text-sm"
               >
@@ -105,17 +152,27 @@ export default function () {
                   alt="avatar"
                   className="w-8 h-8 rounded-full md:w-10 md:h-10"
                 />
-              </li>
-              <li
+              </motion.li>
+              <motion.li
+                whileHover={{
+                  scale: 0.9,
+                  transition: { duration: 0.8 },
+                }}
+                whileTap={{ scale: 0.9 }}
                 onClick={handleLogout}
                 className="p-1 ml-1 font-medium cursor-pointer md:p-2 md:font-bold"
               >
                 <i className="text-lg fa-solid fa-right-from-bracket"></i>
-              </li>
+              </motion.li>
             </>
           ) : (
             <>
-              <li
+              <motion.li
+                whileHover={{
+                  scale: 1.2,
+                  transition: { duration: 0.8 },
+                }}
+                whileTap={{ scale: 0.9 }}
                 onClick={myCart}
                 className="relative p-2 mr-4 text-lg font-medium cursor-pointer md:font-bold"
               >
@@ -123,26 +180,44 @@ export default function () {
                   {cart?.item?.length || 0}
                 </span>
                 <i className="fa-solid fa-cart-shopping"></i>
-              </li>
-              <li
+              </motion.li>
+              <motion.li
+                whileHover={{
+                  scale: 0.9,
+                  transition: { duration: 0.8 },
+                }}
+                whileTap={{ scale: 0.9 }}
                 onClick={login}
                 className="mr-1 text-xs font-medium cursor-pointer md:mr-2 md:font-bold md:text-sm"
               >
                 <i className="fa-solid fa-right-to-bracket mr-[2px]"></i>LOGIN
-              </li>
-              <li
+              </motion.li>
+              <motion.li
+                whileHover={{
+                  scale: 0.9,
+                  transition: { duration: 0.8 },
+                }}
+                whileTap={{ scale: 0.9 }}
                 onClick={register}
                 className="mr-1 text-xs font-medium truncate cursor-pointer md:mr-2 md:font-bold md:text-sm"
               >
                 <i className="fa-solid fa-user-plus mr-[2px]"></i> SIGN UP
-              </li>
+              </motion.li>
             </>
           )}
         </ul>
       </div>
       <div className="block p-1 mr-4 rounded-md md:hidden ">
         <ul className="flex flex-row items-center">
-          <li
+          <motion.li
+            whileHover={{
+              scale: 1.2,
+              transition: { duration: 0.8 },
+              animate: {
+                ease: "easeInOut",
+              },
+            }}
+            whileTap={{ scale: 0.9 }}
             onClick={myCart}
             className="relative p-2 mr-4 text-lg font-medium md:font-bold"
           >
@@ -150,7 +225,7 @@ export default function () {
               {cart?.item?.length || 0}
             </span>
             <i className="fa-solid fa-cart-shopping"></i>
-          </li>
+          </motion.li>
           <li
             className="p-1 transition-all duration-500 rounded-md hover:bg-gray-500 hover:text-white "
             onClick={handleMenu}
